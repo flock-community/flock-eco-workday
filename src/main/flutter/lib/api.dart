59908d6792d345d57c80e05715112e32f067d96a
Kasper Peulen <kpeulen@bol.com>
2019-05-10 16:27:03 +0200

    chore | add holiday

--- src/main/flutter/lib/api.dart
+++ src/main/flutter/lib/api.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flock_eco_holidays/holiday.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import "package:http/http.dart" as http;
 
 const url = 'http://6b81cfd4.ngrok.io/api/';
@@ -12,9 +13,15 @@ class Api {
     return list.map((it) => Holiday.fromJson(it)).toList();
   }
 
-  Future<void> addHoliday(currentUser, Holiday holiday) async {
-    var response = await http.post(url + 'holidays', headers: await currentUser.authHeaders, body: holiday.toJson());
-    print(response);
+  Future<void> addHoliday(GoogleSignInAccount currentUser, Holiday holiday) async {
+    print(holiday);
+    var headers2 = await currentUser.authHeaders;
+    var map = {
+      ...headers2,
+      'Content-Type': 'application/json',
+    };
+    var response = await http.post(url + 'holidays', headers: map, body: json.encode(holiday));
+    print(response.body);
   }
 }
 
