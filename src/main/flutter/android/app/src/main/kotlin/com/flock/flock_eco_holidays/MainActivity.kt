925f375414ec5f16a8851dba17b02127f11b6f29
Kasper Peulen <kpeulen@bol.com>
2019-05-10 11:35:02 +0200

    chore | rename app

---
+++ src/main/flutter/android/app/src/main/kotlin/com/flock/flock_eco_holidays/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.flock.flock_eco_holidays
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
