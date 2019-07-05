b8b89f5237e3d5373c8810a03dca946c9268adb2
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2019-07-05 10:54:11 +0200

    move loaddata

--- src/main/kotlin/community/flock/eco/holidays/Application.kt
+++ src/main/kotlin/community/flock/eco/holidays/Application.kt
@@ -30,42 +30,5 @@ fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
 }
 
-@Component
-class LoadData(
-        val eventRepository: EventRepository
-){
-
-    init {
-        Event(
-                name = "Flock. day",
-                type = EventType.FLOCK_DAY,
-                date = LocalDateTime.of(2019, 7, 19, 0,0)
-        ).save()
-
-        Event(
-                name = "Flock. day",
-                type = EventType.FLOCK_DAY,
-                date = LocalDateTime.of(2019, 7, 5, 0,0)
-        ).save()
-
-        Event(
-                name = "Flock. day",
-                type = EventType.FLOCK_DAY,
-                date = LocalDateTime.of(2019, 6, 21, 0,0)
-        ).save()
-
-        Event(
-                name = "Flock. day",
-                type = EventType.FLOCK_DAY,
-                date = LocalDateTime.of(2019, 6, 7, 0,0)
-        ).save()
-    }
-
-    private fun Event.save() {
-        eventRepository.save(this) //To change body of created functions use File | Settings | File Templates.
-    }
-
-}
-
 
 
---
+++ src/mocks/kotlin/mocks/LoadData.kt
@@ -0,0 +1,44 @@
+package mocks
+
+import community.flock.eco.holidays.model.Event
+import community.flock.eco.holidays.model.EventType
+import community.flock.eco.holidays.repository.EventRepository
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+
+@Component
+class LoadData(
+        val eventRepository: EventRepository
+){
+
+    init {
+        Event(
+                name = "Flock. day",
+                type = EventType.FLOCK_DAY,
+                date = LocalDateTime.of(2019, 7, 19, 0,0)
+        ).save()
+
+        Event(
+                name = "Flock. day",
+                type = EventType.FLOCK_DAY,
+                date = LocalDateTime.of(2019, 7, 5, 0,0)
+        ).save()
+
+        Event(
+                name = "Flock. day",
+                type = EventType.FLOCK_DAY,
+                date = LocalDateTime.of(2019, 6, 21, 0,0)
+        ).save()
+
+        Event(
+                name = "Flock. day",
+                type = EventType.FLOCK_DAY,
+                date = LocalDateTime.of(2019, 6, 7, 0,0)
+        ).save()
+    }
+
+    private fun Event.save() {
+        eventRepository.save(this) //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
