f0af0fd9adff41caaf87f63d51c0fa765c330550
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2019-08-02 11:07:49 +0200

    complete CRUD functions holiday

--- src/main/kotlin/community/flock/eco/holidays/services/HolidayService.kt
+++ src/main/kotlin/community/flock/eco/holidays/services/HolidayService.kt
@@ -1,5 +1,6 @@
 package community.flock.eco.holidays.services
 
+import community.flock.eco.core.utils.toNullable
 import community.flock.eco.feature.user.model.User
 import community.flock.eco.holidays.forms.HolidayForm
 import community.flock.eco.holidays.model.DayOff
@@ -15,7 +16,7 @@ import java.util.*
 class HolidayService(
         val holidayRepository: HolidayRepository) {
 
-    fun findById(id:Long): Optional<Holiday> = holidayRepository.findById(id)
+    fun findById(id: Long): Optional<Holiday> = holidayRepository.findById(id)
 
     fun create(form: HolidayForm, user: User): Holiday {
         form.validate()
@@ -36,21 +37,27 @@ class HolidayService(
 
     fun update(id: Long, form: HolidayForm): Holiday {
         form.validate()
-        return Holiday(
-                description = form.description,
-                from = form.from,
-                to = form.to,
-                dayOff = form.dayOff.mapIndexed { index, hours ->
-                    DayOff(
-                            type = DayType.HOLIDAY,
-                            date = form.from.plusDays(index.toLong()),
-                            hours = hours
-                    )
-                }.toSet())
-                .save()
+        return holidayRepository.findById(id)
+                .toNullable()
+                ?.let { holiday ->
+                    Holiday(
+                            description = form.description,
+                            from = form.from,
+                            to = form.to,
+                            dayOff = form.dayOff.mapIndexed { index, hours ->
+                                DayOff(
+                                        type = DayType.HOLIDAY,
+                                        date = form.from.plusDays(index.toLong()),
+                                        hours = hours
+                                )
+                            }.toSet(),
+                            user = holiday.user)
+                }
+                ?.save()
+                ?: throw java.lang.RuntimeException("Cannot update Holiday")
     }
 
-    fun delete(id:Long) = holidayRepository.deleteById(id)
+    fun delete(id: Long) = holidayRepository.deleteById(id)
 
     private fun Holiday.save() = holidayRepository
             .save(this)
