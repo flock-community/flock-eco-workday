a57fb89bcd51e1d90036e71a34741dd297bb83ea
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-11-19 17:05:06 +0100

    add styles
    + import makeStyle from material-ui
    + create useStyle
      + add root styles and override class properties
      + add card styles for card container
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/main/react/features/person/PersonFeature.js
+++ src/main/react/features/person/PersonFeature.js
@@ -1,4 +1,17 @@
 import React from "react"
+import {makeStyles} from "@material-ui/styles"
+const useStyles = makeStyles(() => ({
+  root: {
+    // override .MuiGrid-spacing-xs-1 class properties
+    margin: 0,
+    width: "auto",
+  },
+  card: {
+    margin: 16,
+    maxWidth: 1200, // should represent a @screen break-point
+    width: "100%",
+  },
+}))
 
 export const PersonFeature = () => {
     return (<div>Mau</div>)
