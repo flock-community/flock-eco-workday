0823334422f220649270d6523963fdf98cf089fa
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-11-19 17:51:00 +0100

    create PersonTable
    + add styles via makeStyles @material-ui
    + destruct props
    + create PropTypes for props passed/required
    + add handleChangePage for table pagination
    + add handleChangeRowsPerPage for table pagination
    + use PersonTableHead component
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

---
+++ src/main/react/features/person/table/PersonTable.js
@@ -0,0 +1,88 @@
+import React, {useState} from "react"
+import PropTypes from "prop-types"
+import {Link as RouterLink} from "react-router-dom"
+import {
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+  Link,
+  TablePagination,
+} from "@material-ui/core"
+import {PersonTableHead} from "./PersonTableHead"
+import {makeStyles} from "@material-ui/styles"
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+  },
+  tableWrapper: {
+    overflow: "auto",
+  },
+})
+
+export const PersonTable = props => {
+  const {persons} = props
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(10)
+  const classes = useStyles()
+
+  const handleChangePage = (_, newPage) => {
+    // TODO: query user endpoint to retrieve user list for page
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = event => {
+    // TODO: query user GET endpoint with limit param and page=0
+    const rowsPerPage = event.target.value
+    setRowsPerPage(+rowsPerPage)
+    setPage(0)
+  }
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.tableWrapper}>
+        <Table>
+          <PersonTableHead></PersonTableHead>
+          <TableBody>
+            {persons.map((person, index) => {
+              return (
+                <TableRow key={index} hover>
+                  <TableCell component="th" scope="row">
+                    <Link
+                      component={RouterLink}
+                      to={`/person/${index}`}
+                      underline="none"
+                    >
+                      {person.firstname} {person.lastname}
+                    </Link>
+                  </TableCell>
+                  <TableCell align="left">{person.email}</TableCell>
+                  <TableCell align="left">{person.active}</TableCell>
+                  <TableCell align="left">{person.holidays}</TableCell>
+                  <TableCell align="left">{person.clients}</TableCell>
+                  <TableCell align="left">{person.hours}</TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </div>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={persons.length}
+        // remove labelDisplayRows by replacing it with an empty return
+        labelDisplayedRows={(from, to, count) => {}}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </div>
+  )
+}
+
+PersonTable.propTypes = {
+  persons: PropTypes.array.isRequired,
+}
