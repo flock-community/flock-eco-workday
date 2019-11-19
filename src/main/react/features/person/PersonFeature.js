0159e431e7815fb5fbefe8f80cafca7874cab1f9
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-11-19 17:52:31 +0100

    add dummy persons
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/main/react/features/person/PersonFeature.js
+++ src/main/react/features/person/PersonFeature.js
@@ -1,5 +1,35 @@
 import React from "react"
 import {makeStyles} from "@material-ui/styles"
+const persons = [
+  {
+    firstname: "Workday",
+    lastname: "Flock",
+    email: "workday@flock-se.com",
+    active: "today",
+    holidays: 14,
+    clients: 1,
+    hours: 24,
+  },
+  {
+    firstname: "Workday",
+    lastname: "Flock",
+    email: "workday@flock-se.com",
+    active: "yesterday",
+    holidays: 24,
+    clients: 1,
+    hours: 14,
+  },
+  {
+    firstname: "Workday",
+    lastname: "Flock",
+    email: "workday@flock-se.com",
+    active: "day before yesterday",
+    holidays: 14,
+    clients: 1,
+    hours: 4,
+  },
+]
+
 const useStyles = makeStyles(() => ({
   root: {
     // override .MuiGrid-spacing-xs-1 class properties
