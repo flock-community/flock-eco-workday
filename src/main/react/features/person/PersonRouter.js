e0b8c89e82aa7f98e3b5bbfd4eb961271ab51bcf
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-11-20 14:24:44 +0100

    create PersonRouter
    + add default route to "index" page which is represented by the table of all users
    + add detail route for the user which is selected from the table
    + add PropTypes for clarity
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

---
+++ src/main/react/features/person/PersonRouter.js
@@ -0,0 +1,24 @@
+import React from "react"
+import PropTypes from "prop-types"
+import {Route, Switch, NavLinks} from "react-router-dom"
+import {PersonTable} from "./table/PersonTable"
+import {PersonDetails} from "./PersonDetails"
+import {PersonSubNav} from "./PersonSubNav"
+
+export const PersonRouter = props => {
+  const {match} = props
+  const {path, url, isExact, params} = match
+
+  return (
+    <Switch>
+      <Route exact path={url} component={PersonTable}></Route>
+      <Route exact path={`${url}/id/:personId`} component={PersonDetails}></Route>
+    </Switch>
+  )
+}
+
+PersonRouter.propTypes = {
+  match: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+  }),
+}
