f84a86f0c02efe2654fca8f64947d18fdbc58cbb
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-11-21 11:57:24 +0100

    simplify return statement of getById function
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/main/react/features/person/PersonService.js
+++ src/main/react/features/person/PersonService.js
@@ -4,9 +4,7 @@ const getById = id => {
   const opts = {
     method: "GET",
   }
-  return fetch(`${path}/${id}`, opts).then(res => {
-    return res.json()
-  })
+  return fetch(`${path}/${id}`, opts).then(res => res.json())
 }
 
 export const PersonService = {
