296cb86b3098b51d35f3c29dc9ab2feadbc826bc
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-11-21 11:53:49 +0100

    add PersonService to retrieve user data
    + iterate over firstname, lastname, email of user
    + add static id for as getById param
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/main/react/features/person/PersonDetails.js
+++ src/main/react/features/person/PersonDetails.js
@@ -1,8 +1,24 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
+import {PersonService} from "./PersonService"
+import {Paper} from "@material-ui/core"
 
 export const PersonDetails = props => {
   const {match} = props
   const {path, url, isExact, params} = match
+  const [person, setPerson] = useState({})
 
-  return <h1>Hello UserID: {params.personId}</h1>
+  useEffect(() => {
+    PersonService.getById(57).then(person => setPerson(person))
+  }, [])
+
+  return (
+    <Paper>
+      <h1>Hello UserID: {params.personId}</h1>
+      {["firstname", "lastname", "email"].map((key, idx) => (
+        <p key={idx}>
+          {key}: {person[key]}
+        </p>
+      ))}
+    </Paper>
+  )
 }
