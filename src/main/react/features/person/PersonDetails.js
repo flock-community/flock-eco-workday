07200aad5b6a270ab7af6408925b386cd9c5f5be
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-11-21 15:38:32 +0100

    add PersonWidget
    + import requirements
    + add empty style classes
    + replace template with grid item
    + add PersonWidget and pass person
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/main/react/features/person/PersonDetails.js
+++ src/main/react/features/person/PersonDetails.js
@@ -1,26 +1,27 @@
 import React, {useEffect, useState} from "react"
 import PropTypes from "prop-types"
 import {PersonService} from "./PersonService"
-import {Paper} from "@material-ui/core"
+import {Grid} from "@material-ui/core"
+import {makeStyles} from "@material-ui/styles"
+import {PersonWidget} from "./widgets/PersonWidget"
+
+const useStyle = makeStyles(() => ({}))
 
 export const PersonDetails = props => {
   const {match} = props
   const {path, url, isExact, params} = match
   const [person, setPerson] = useState({})
+  const classes = useStyle()
 
   useEffect(() => {
     PersonService.getById(params.personId).then(person => setPerson(person))
   }, [])
 
   return (
-    <Paper>
-      <h1>Hello UserID: {params.personId}</h1>
-      {["firstname", "lastname", "email"].map((key, idx) => (
-        <p key={idx}>
-          {key}: {person[key]}
-        </p>
-      ))}
-    </Paper>
+    // <Grid container> wrapper is defined @PersonFeature
+    <Grid item xs={12} sm={6}>
+      <PersonWidget person={person} />
+    </Grid>
   )
 }
 
