b720ba3fadcdd5d7232b216baee2bb825b321024
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-11-27 17:50:48 +0100

    create AddActionFab Component
    + copy styles from ClientFeature which uses the Fab button
    + create HOC from Fab with custom styles
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

---
+++ src/main/react/components/FabButtons.js
@@ -0,0 +1,30 @@
+import React from "react"
+import AddIcon from "@material-ui/icons/Add"
+import {Fab} from "@material-ui/core"
+import {makeStyles} from "@material-ui/styles"
+
+const useStyles = makeStyles(() => ({
+  root: {
+    position: "absolute",
+    bottom: 25,
+    right: 25,
+  },
+}))
+
+/** AddActionFab
+ * creates a floating action button with the AddIcon from @material-ui/icons
+ * @param {*} props
+ */
+const AddActionFab = props => {
+  const classes = useStyles()
+
+  return (
+    <Fab className={classes.root} {...props}>
+      <AddIcon />
+    </Fab>
+  )
+}
+
+AddActionFab.propTypes = {}
+
+export {AddActionFab}
