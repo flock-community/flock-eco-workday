daf991e64a3019509a2a826f70817075a77ebd49
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-11-27 17:28:53 +0100

    add post func
    + create post function
    + posts item to api endpoint persons
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/main/react/features/person/PersonService.js
+++ src/main/react/features/person/PersonService.js
@@ -18,7 +18,20 @@ const getById = id => {
   return fetch(`${path}/${id}`, opts).then(res => res.json())
 }
 
+const post = item => {
+  const opts = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(item),
+  }
+
+  return fetch(`${path}`, opts).then(res => res.json())
+}
+
 export const PersonService = {
   getAll,
   getById,
+  post,
 }
