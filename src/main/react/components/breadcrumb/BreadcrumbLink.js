e20e0b137079e1e76b18b6a8505c320479492ede
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-11-29 15:31:16 +0100

    create BreadcrumbLink
    + builds on top of react-router-dom Link
    + adds breadcrumb class depending on breadcrumb props
    + add propTypes
    + add class styles from materialcss for breadcrumbs
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

---
+++ src/main/react/components/breadcrumb/BreadcrumbLink.js
@@ -0,0 +1,16 @@
+import React from "react"
+import PropTypes from "prop-types"
+import {Link} from "react-router-dom"
+
+export const BreadcrumbLink = props => {
+  const {breadcrumbs, ...rest} = props
+  const classes = breadcrumbs
+    ? "MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorInherit"
+    : ""
+
+  return <Link className={classes} {...rest} />
+}
+
+BreadcrumbLink.propTypes = {
+  breadcrumbs: PropTypes.bool,
+}
