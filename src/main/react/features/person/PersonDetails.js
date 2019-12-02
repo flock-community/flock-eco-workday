d0260e7b97540b2e509d02b96f91220475313df0
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-12-02 12:33:43 +0100

    add PersonDetails details
    + add grid layout
    + add html structure
    + use Feed item for detail listing
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/main/react/features/person/PersonDetails.js
+++ src/main/react/features/person/PersonDetails.js
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from "react"
 import {useRouteMatch} from "react-router-dom"
-import {Grid} from "@material-ui/core"
+import {Grid, Paper, Typography} from "@material-ui/core"
 import {makeStyles} from "@material-ui/styles"
 import {PersonService} from "./PersonService"
 import {PersonWidget} from "./widgets/PersonWidget"
 import {useBreadcrumbs} from "../../components/breadcrumb"
+import {Feed} from "./widgets/Feed"
 
 const useStyle = makeStyles(() => ({
   root: {margin: "-8px"},
@@ -33,8 +34,27 @@ export const PersonDetails = () => {
 
   return (
     // <Grid container> wrapper is defined @PersonFeature
-    <Grid item xs={12} sm={6}>
-      <PersonWidget person={person} />
+    <Grid container item xs={12} spacing={2} className={classes.root}>
+      <Grid item xs={12} sm={4}>
+        <PersonWidget person={person} />
+      </Grid>
+      <Grid item xs={12} sm={8}>
+        <div>
+          <Feed title="User Information" item={person}>
+            <ul>
+              <li>firstname: {person.firstname}</li>
+              <li>lastname: {person.lastname}</li>
+              <li>email: {person.email}</li>
+            </ul>
+          </Feed>
+          {/* <Paper className={classes.defaultPadding}>
+            <Typography variant="caption">User Information Details</Typography>
+            <Typography variant="body1" component="p">
+              Hello
+            </Typography>
+          </Paper> */}
+        </div>
+      </Grid>
     </Grid>
   )
 }
