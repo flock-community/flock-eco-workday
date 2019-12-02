555fdb18e027c8742f3d9c2a567aff4f47b3ac94
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-12-02 11:32:07 +0100

    create delete function
    + add the delete function to send to the api endpoint of persons
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/main/react/features/person/PersonService.js
+++ src/main/react/features/person/PersonService.js
@@ -30,8 +30,17 @@ const post = item => {
   return fetch(`${path}`, opts).then(res => res.json())
 }
 
+
+const del = id => {
+  const opts = {
+    method: "DELETE",
+  }
+  return fetch(`${path}/${id}`, opts).then(res => res.json())
+}
+
 export const PersonService = {
   getAll,
   getById,
   post,
+  delete: del,
 }
