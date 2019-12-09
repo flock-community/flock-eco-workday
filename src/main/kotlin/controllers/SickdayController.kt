9ef3a1e333c5522184251544486b70090873d573
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-12-09 19:02:23 +0100

    update SickdayController
    + add getAll fun with RequestParams status and code
    + add post fun with RequestBody SickdayForm
    + add put fun with PathVariable code and RequestBody SickdayForm
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/main/kotlin/controllers/SickdayController.kt
+++ src/main/kotlin/controllers/SickdayController.kt
@@ -1,15 +1,33 @@
 package community.flock.eco.workday.controllers
 
-import community.flock.eco.feature.user.repositories.UserRepository
-import community.flock.eco.workday.repository.PeriodRepository
-import community.flock.eco.workday.services.HolidayService
+import community.flock.eco.core.utils.toResponse
+import community.flock.eco.workday.filters.SickdayFilters
+import community.flock.eco.workday.forms.SickdayForm
+import community.flock.eco.workday.services.SickdayService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/sickdays")
 class SickdayController(
-    private val userRepository: UserRepository,
-    private val periodRepository: PeriodRepository,
-    private val holidayService: HolidayService
-)
+    private val service: SickdayService
+) {
+    @GetMapping
+    fun getAll(@RequestParam status: SickdayFilters?, @RequestParam code: Int?) = service
+        .findAll(status, code)
+        .toResponse()
+
+    @PostMapping
+    fun post(@RequestBody form: SickdayForm) = service.create(form).toResponse()
+
+    @PutMapping("/{code}")
+    fun put(@PathVariable code: String, @RequestBody form: SickdayForm) = service
+        .update(code, form)
+        .toResponse()
+}
