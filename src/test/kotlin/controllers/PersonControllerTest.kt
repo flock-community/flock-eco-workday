de29aac10be486b884d38b4513e20a857d40d509
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-12-10 19:21:51 +0100

    create test to retrieve a person via code GET-method
    + import JsonNode
    + remove import Person model
    + remove import PersonService
    + import PersonForm to create a Person via form
    
    + create a person via PersonForm and POST-method
    + use created person´s code to set the get param code
    + expect that person to be equal
    + add utils function to convert mvcMockResult
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/test/kotlin/controllers/PersonControllerTest.kt
+++ src/test/kotlin/controllers/PersonControllerTest.kt
@@ -1,12 +1,12 @@
 package community.flock.eco.workday.controllers
 
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import community.flock.eco.feature.user.forms.UserAccountPasswordForm
 import community.flock.eco.feature.user.services.UserAccountService
 import community.flock.eco.feature.user.services.UserSecurityService
 import community.flock.eco.workday.Application
-import community.flock.eco.workday.model.Person
-import community.flock.eco.workday.services.PersonService
+import community.flock.eco.workday.forms.PersonForm
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -90,6 +90,44 @@ class PersonControllerTest {
     }
 
     @Test
+    fun `should get a person by code via GET-method`() {
+        /* DRY-Block */
+        val user = UserAccountPasswordForm(
+            email = "admin@reynholm-instudries.co.uk",
+            name = "Administrator",
+            authorities = setOf(),
+            password = "admin")
+            .run { userAccountService.createUserAccountPassword(this) }
+            .run { UserSecurityService.UserSecurityPassword(this) }
+            .run { user(this) }
+
+        val personForm = PersonForm(firstname = "Morris", lastname = "Moss", email = null)
+
+        // need this user to compare generated fields
+        // create a person so one can query that person via the PersonCode
+        var person: JsonNode? = null
+        mvc.perform(post(baseUrl).with(user)
+            .content(mapper.writeValueAsString(personForm))
+            .contentType(APPLICATION_JSON)
+            .accept(APPLICATION_JSON))
+            .andReturn()
+            .response
+            .contentAsString
+            .apply { person = mapper.readTree(this) }
+
+        fun person(key: String): String = person!!.get(key).textValue()
+        /* DRY-Block */
+
+        mvc.perform(get("$baseUrl/${person("code")}")
+            .with(user)
+            .accept(APPLICATION_JSON))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+            .andExpect(jsonPath("\$.code").exists())
+            .andExpect(jsonPath("\$.code").isString)
+            .andExpect(jsonPath("\$.code").value(person("code")))
+            .andExpect(jsonPath("\$.firstname").value(person("firstname")))
+            .andExpect(jsonPath("\$.lastname").value(person("lastname")))
     }
 
     @Test
