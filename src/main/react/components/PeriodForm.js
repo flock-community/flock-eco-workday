d6b6fe055a980813c8500e410fb70f257208aca4
Michiel <michiel.doorn@gmail.com>
2019-12-20 15:57:01 +0100

    Add small todoList, bugfix: number input in PeriodForm hour field.

--- src/main/react/components/PeriodForm.js
+++ src/main/react/components/PeriodForm.js
@@ -137,7 +137,7 @@ export function PeriodForm({value, onChange}) {
   const handleDayChange = it => ev => {
     const val = {
       ...days,
-      [it]: parseInt(ev.target, 10),
+      [it]: Math.max(Math.min(parseInt(ev.target.value, 10), 8), 0),
     }
     setState({
       dates: [dates[0], dates[1]],
