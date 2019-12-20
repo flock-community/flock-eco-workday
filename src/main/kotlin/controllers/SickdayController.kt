df443195f64e9c73cd973d137f5b2d8298cfa674
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-12-20 10:18:23 +0100

    create delete
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/main/kotlin/controllers/SickdayController.kt
+++ src/main/kotlin/controllers/SickdayController.kt
@@ -4,6 +4,7 @@ import community.flock.eco.core.utils.toResponse
 import community.flock.eco.workday.filters.SickdayFilters
 import community.flock.eco.workday.forms.SickdayForm
 import community.flock.eco.workday.services.SickdayService
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -30,4 +31,7 @@ class SickdayController(
     fun put(@PathVariable code: String, @RequestBody form: SickdayForm) = service
         .update(code, form)
         .toResponse()
+
+    @DeleteMapping("/{code}")
+    fun delete(@PathVariable code: String) = service.delete(code).toResponse()
 }
