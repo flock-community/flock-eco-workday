dae1a9d92aa755c7aae2f551b7a2841a5f01c57a
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-12-20 10:16:45 +0100

    remove custom repository query
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/main/kotlin/repository/SickdayRepositoryCustom.kt
+++
@@ -1,9 +0,0 @@
-package community.flock.eco.workday.repository
-
-import community.flock.eco.workday.filters.SickdayFilters
-import org.springframework.stereotype.Repository
-
-@Repository
-interface SickdayRepositoryCustom {
-    fun filterBy(status: SickdayFilters?, personCode: Int?): Iterable<Any?>
-}
