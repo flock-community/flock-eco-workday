1a82efdea2e25f72b81536cf24831bd9c983edca
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-12-20 10:17:03 +0100

    add findAllByStatusAndPersonCode
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/main/kotlin/repository/SickdayRepository.kt
+++ src/main/kotlin/repository/SickdayRepository.kt
@@ -1,6 +1,7 @@
 package community.flock.eco.workday.repository
 
 import community.flock.eco.workday.model.Sickday
+import community.flock.eco.workday.model.SickdayStatus
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
@@ -8,4 +9,5 @@ import org.springframework.stereotype.Repository
 interface SickdayRepository : CrudRepository<Sickday, Long>, SickdayRepositoryCustom {
     fun findByCode(code: String): Sickday?
     fun deleteByCode(code: String)
+    fun findAllByStatusAndPersonCode(status: SickdayStatus, personCode: String): Iterable<Sickday>
 }
