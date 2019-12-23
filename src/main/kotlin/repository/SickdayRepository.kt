6300304834be670941d21a455f5ccfc44489c818
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-12-23 14:44:34 +0100

    add additional findAll queries
    + add findAllByStatus to query only by status field
    + add findAllByPersonCode to query only by personCode
    + remove SickdayRepositoryCustom
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/main/kotlin/repository/SickdayRepository.kt
+++ src/main/kotlin/repository/SickdayRepository.kt
@@ -6,8 +6,10 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface SickdayRepository : CrudRepository<Sickday, Long>, SickdayRepositoryCustom {
+interface SickdayRepository : CrudRepository<Sickday, Long> {
     fun findByCode(code: String): Sickday?
     fun deleteByCode(code: String)
-    fun findAllByStatusAndPersonCode(status: SickdayStatus, personCode: String): Iterable<Sickday>
+    fun findAllByStatusAndPersonCode(status: SickdayStatus?, personCode: String?): Iterable<Sickday>
+    fun findAllByStatus(status: SickdayStatus): Iterable<Sickday>
+    fun findAllByPersonCode(personCode: String): Iterable<Sickday>
 }
