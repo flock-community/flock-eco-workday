fa0a9cc9ce5df0d89255ea394c9f27db666edf6a
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-12-24 10:58:56 +0100

    replace DialogTitle, DialogAction with CustomDialogs
    + remove unused ui components
    + remove styles
    + add DialogFooter, DialogHeader
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/main/react/features/person/PersonDialog.js
+++ src/main/react/features/person/PersonDialog.js
@@ -1,51 +1,16 @@
 import React from "react"
 import PropTypes from "prop-types"
-import clsx from "clsx"
-import {
-  Dialog,
-  Button,
-  DialogTitle,
-  DialogContent,
-  Typography,
-  DialogActions,
-  Divider,
-} from "@material-ui/core"
-import {PersonAdd, Close} from "@material-ui/icons"
+import {Dialog, Divider, DialogContent} from "@material-ui/core"
+import {PersonAdd} from "@material-ui/icons"
 import {makeStyles} from "@material-ui/styles"
 import {PersonForm, PERSON_FORM_ID} from "./PersonForm"
 import {PersonService} from "./PersonService"
 import {usePerson} from "./context/PersonContext"
 import {isEmptyObject} from "../../utils/validation"
 import {TransitionSlider} from "../../components/transitions/Slide"
+import {DialogHeader, DialogFooter} from "../../components/dialog"
 
 const useStyles = makeStyles(() => ({
-  flex: {
-    display: "flex",
-  },
-  flexDirectionRowReverse: {
-    flexDirection: "row-reverse",
-  },
-  justifyContentEnd: {
-    justifyContent: "end",
-  },
-  dialogTitle: {
-    alignItems: "center",
-    display: "flex",
-  },
-  personAddIcon: {
-    height: 40,
-    width: 40,
-  },
-  closeBtn: {
-    alignSelf: "start",
-    marginLeft: "auto",
-    minWidth: "unset", // override material button property
-  },
-  titleText: {
-    display: "flex",
-    flexDirection: "column",
-    marginLeft: "1rem",
-  },
   dialogContent: {
     margin: "auto",
     maxWidth: 700, // should be a decent medium-sized breakpoint
@@ -66,7 +31,7 @@ export const PersonDialog = props => {
     onClose()
   }
 
-  const handleSubmit = (values, actions) => {
+  const handleSubmit = values => {
     if (isEmptyObject(person)) {
       PersonService.post(values)
         .then(() => successfulSubmit())
@@ -86,41 +51,17 @@ export const PersonDialog = props => {
       TransitionComponent={TransitionSlider}
       TransitionProps={{direction: "right"}}
     >
-      <DialogTitle>
-        <div className={classes.dialogTitle}>
-          <PersonAdd className={classes.personAddIcon} />
-          <div className={classes.titleText}>
-            <Typography variant="body1">Create Person</Typography>
-            <Typography variant="caption">
-              Fill out the form to create a person
-            </Typography>
-          </div>
-          <Button className={classes.closeBtn} onClick={onClose}>
-            <Close />
-          </Button>
-        </div>
-      </DialogTitle>
+      <DialogHeader
+        icon={<PersonAdd className={classes.personAddIcon} />}
+        headline="Create Person"
+        subheadline="Fill out the form to create a person"
+        onClose={onClose}
+      />
       <DialogContent className={classes.dialogContent}>
         <PersonForm item={person} onSubmit={handleSubmit} />
       </DialogContent>
       <Divider />
-      {/* reverse the DialogActions with `flex-direction: row-reverse` to enable
-       * navigating via tab to the save button first, but position the save button last
-       */}
-      <DialogActions
-        className={clsx(
-          classes.flex,
-          classes.flexDirectionRowReverse,
-          classes.justifyContentEnd
-        )}
-      >
-        <Button type="submit" form={PERSON_FORM_ID} color="primary">
-          Save
-        </Button>
-        <Button color="secondary" onClick={onClose}>
-          Cancel
-        </Button>
-      </DialogActions>
+      <DialogFooter formId={PERSON_FORM_ID} onClose={onClose} />
     </Dialog>
   )
 }
