901ce75a35cf1d19d05f309fe35862cea5146a6d
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-12-30 16:19:33 +0100

    update Person parameters
    + iterate over userData in init call
    + create a person for every user in userData
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/develop/kotlin/mocks/LoadPersonData.kt
+++ src/develop/kotlin/mocks/LoadPersonData.kt
@@ -1,5 +1,7 @@
 package community.flock.eco.workday.mocks
 
+import community.flock.eco.feature.user.model.User
+import community.flock.eco.feature.user.repositories.UserRepository
 import community.flock.eco.workday.model.Person
 import community.flock.eco.workday.repository.PersonRepository
 import org.springframework.context.annotation.Profile
@@ -8,11 +10,14 @@ import org.springframework.stereotype.Component
 @Component
 @Profile("local")
 class LoadPersonData(
-    private val repository: PersonRepository
+    userData: LoadUserData,
+    private val repository: PersonRepository,
+    private val userRepo: UserRepository
 ) {
     val data: MutableSet<Person> = mutableSetOf()
 
-    /** add save() func to model Person
+    /**
+     * add save() func to model Person
      * call the PersonRepository´s save function passing the Person
      */
     fun Person.save(): Person = repository
@@ -21,27 +26,38 @@ class LoadPersonData(
             data.add(it)
         }
 
-    /** createPerson() func
+    /**
+     * createPerson() func
      * generate a Person model from given input values
      *
      * @param firstname firstname of the person
      * @param lastname lastname of the person
      * @param email (optional) the email of the person.
+     * @param position (optional) job description or position of the user
+     * @param user (optional) User model the person is attached to
      */
-    private final fun createPerson(firstname: String, lastname: String, email: String = "") = Person(
+    private fun createPerson(
+        firstname: String,
+        lastname: String,
+        email: String = "",
+        position: String? = null,
+        user: User? = null
+    ) = Person(
             firstname = firstname,
             lastname = lastname,
-            email = email
+            email = email,
+            position = position,
+            user = user
         ).save()
 
     /**
      * Initialize the users by calling the createPerson() func
+     * iterate over userData and create a person for every user in userData
      */
     init {
-        createPerson("Tommy", "Dog")
-        createPerson("Ieniemienie", "Mouse")
-        createPerson("Pino", "Woodpecker")
-        createPerson("Bert", "Muppets")
-        createPerson("Ernie", "Muppets")
+        val lastnames = arrayOf("Dog", "Mouse", "Woodpecker", "Muppets", "Muppets")
+        userData.data.forEachIndexed {
+            idx, user -> createPerson(firstname = user.name!!, lastname = lastnames[idx], user = user)
+        }
     }
 }
