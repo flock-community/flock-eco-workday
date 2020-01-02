1212e3b0a81d5cbdd0b40f41aacf9c04e8e57b34
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2020-01-02 19:42:48 +0100

    fix user authorization
    + add findUser to evaluate the current user
    + add person resolve for endpoints with READ/WRITE access
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/main/kotlin/controllers/SickdayController.kt
+++ src/main/kotlin/controllers/SickdayController.kt
@@ -1,9 +1,10 @@
 package community.flock.eco.workday.controllers
 
 import community.flock.eco.core.utils.toResponse
+import community.flock.eco.feature.user.model.User
+import community.flock.eco.feature.user.services.UserService
 import community.flock.eco.workday.authorities.SickdayAuthority
 import community.flock.eco.workday.forms.SickdayForm
-import community.flock.eco.workday.model.Person
 import community.flock.eco.workday.model.Sickday
 import community.flock.eco.workday.model.SickdayStatus
 import community.flock.eco.workday.services.PersonService
@@ -27,7 +28,8 @@ import org.springframework.web.server.ResponseStatusException
 @RequestMapping("/api/sickdays")
 class SickdayController(
     private val service: SickdayService,
-    private val personService: PersonService
+    private val personService: PersonService,
+    private val userService: UserService
 ) {
     @GetMapping
     @PreAuthorize("hasAuthority('SickdayAuthority.READ')")
@@ -36,11 +38,14 @@ class SickdayController(
         @RequestParam(required = false) code: String?,
         principal: Principal
     ): ResponseEntity<Iterable<Sickday>> = principal
-        .findPerson()
+        .findUser()
         ?.let {
             return@let when {
                 it.isAdmin() -> service.findAll(status, code)
-                else -> service.findAll(status, it.code)
+                else -> {
+                    val person = personService.findByUserCode(it.code) ?: throw ResponseStatusException(UNAUTHORIZED)
+                    service.findAll(status, person.code)
+                }
             }.toResponse()
         }
         ?: throw ResponseStatusException(UNAUTHORIZED)
@@ -48,11 +53,14 @@ class SickdayController(
     @GetMapping("/{code}")
     @PreAuthorize("hasAuthority('SickdayAuthority.READ')")
     fun findByCode(@PathVariable code: String, principal: Principal): ResponseEntity<Sickday> = principal
-        .findPerson()
+        .findUser()
         ?.let {
             return@let when {
                 it.isAdmin() -> service.findByCode(code)
-                else -> service.findByCode(it.code)
+                else -> {
+                    val person = personService.findByUserCode(it.code) ?: throw ResponseStatusException(UNAUTHORIZED)
+                    service.findByCode(person.code)
+                }
             }.toResponse()
         }
         ?: throw ResponseStatusException(UNAUTHORIZED)
@@ -60,11 +68,14 @@ class SickdayController(
     @PostMapping
     @PreAuthorize("hasAuthority('SickdayAuthority.WRITE')")
     fun post(@RequestBody form: SickdayForm, principal: Principal) = principal
-        .findPerson()
+        .findUser()
         ?.let {
             // if user has no Admin Authorities the forms personCode is updated to the personCode of the posting user
             // meaning that the user can only create its own sickdays, preventing creations of other users sickdays
-            if (it.isNotAdmin()) form.copy(personCode = it.code)
+            if (it.isNotAdmin()) {
+                val person = personService.findByUserCode(it.code) ?: throw ResponseStatusException(UNAUTHORIZED)
+                form.copy(personCode = person.code)
+            }
             return@let service.create(form).toResponse()
         }
         ?: throw ResponseStatusException(UNAUTHORIZED)
@@ -76,11 +87,14 @@ class SickdayController(
         @RequestBody form: SickdayForm,
         principal: Principal
     ) = principal
-        .findPerson()
+        .findUser()
         ?.let {
             // if user has no Admin Authorities the forms personCode is updated to the personCode of the posting user
             // meaning that the user can only update its own sickdays, preventing updating of other users sickdays
-            if (it.isNotAdmin()) form.copy(personCode = it.code)
+            if (it.isNotAdmin()) {
+                val person = personService.findByUserCode(it.code) ?: throw ResponseStatusException(UNAUTHORIZED)
+                form.copy(personCode = person.code)
+            }
             return@let service.update(code, form).toResponse()
         }
         ?: throw ResponseStatusException(UNAUTHORIZED)
@@ -88,7 +102,7 @@ class SickdayController(
     @DeleteMapping("/{code}")
     @PreAuthorize("hasAuthority('SickdayAuthority.ADMIN')")
     fun delete(@PathVariable code: String, principal: Principal) = principal
-        .findPerson()
+        .findUser()
         ?.let {
             when {
                 it.isAdmin() -> return@let service.delete(code).toResponse()
@@ -99,20 +113,20 @@ class SickdayController(
 
     // *-- utility functions --*
     /**
-     * add findPerson() function to Principal
-     * @return <code>Person?</code> if a person can be found with given user code in the db
+     * add findUser() function to Principal
+     * @return <code>User?</code> a user if found with given user code in the db
      */
-    private fun Principal.findPerson(): Person? = personService.findByUserCode(this.name)
+    private fun Principal.findUser(): User? = userService.findByCode(this.name)
 
     /**
      * Evaluate if user has admin authorities on Sickday
      * @return <code>true</code> if user is admin or has admin authorities
      */
-    private fun Person.isAdmin(): Boolean = this.user?.authorities?.contains(SickdayAuthority.ADMIN.toName()) ?: false
+    private fun User.isAdmin(): Boolean = this.authorities.contains(SickdayAuthority.ADMIN.toName())
 
     /**
      * Evaluate if user has no admin authorities on Sickday
      * @return <code>true</code> if user is not admin or has admin authorities
      */
-    private fun Person.isNotAdmin(): Boolean = !this.isAdmin()
+    private fun User.isNotAdmin(): Boolean = !this.isAdmin()
 }
