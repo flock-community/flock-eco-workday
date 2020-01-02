8761191cff4091632855c942fae97fd71ca52424
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2020-01-02 20:57:54 +0100

    create AssignmentRepositoryTest
    + add empty dummy test
    + add Intellij idea runConfiguration for AssignmentRepositoryTest
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

---
+++ src/test/kotlin/repository/AssignmentRepositoryTest.kt
@@ -0,0 +1,18 @@
+package community.flock.eco.workday.repository
+
+import community.flock.eco.workday.ApplicationConfiguration
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@ContextConfiguration(classes = [ApplicationConfiguration::class])
+@DataJpaTest
+@AutoConfigureTestDatabase
+class AssignmentRepositoryTest {
+    @Test
+    fun `empty dummy test`() {}
+}
