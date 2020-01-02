323887f07a8b7e0533ab6d9f445fff661faa6712
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2020-01-02 18:43:59 +0100

    fix ktlint
    + run ktlint and fix issues
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/main/kotlin/config/SwaggerConfig.kt
+++ src/main/kotlin/config/SwaggerConfig.kt
@@ -3,14 +3,12 @@ package community.flock.eco.workday.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
+import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
-import springfox.documentation.builders.PathSelectors
-import springfox.documentation.builders.PathSelectors.regex
 import springfox.documentation.swagger2.annotations.EnableSwagger2
-
 
 @Configuration
 @EnableSwagger2
@@ -22,7 +20,6 @@ class SwaggerConfig : WebMvcConfigurationSupport() {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-
     }
 
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
