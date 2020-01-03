a76a6205092f31d7c2ffb41f2a4b74234fe79fd8
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2020-01-03 15:17:49 +0100

    add schema
    + create transform functions toPerson, toPersonForm
    + create schema PERSON_SCHEMA, PERSON_FORM_SCHEMA
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

---
+++ src/main/react/features/person/schema.js
@@ -0,0 +1,45 @@
+// import * as Yup from "yup"
+import {object, string} from "yup"
+
+// eslint-disable-next-line no-underscore-dangle
+const _defaultObject = {
+  code: string(),
+  firstname: string()
+    .required()
+    .default(""),
+  lastname: string()
+    .required()
+    .default(""),
+  email: string()
+    .email()
+    .default(""),
+  position: string().default(""),
+}
+
+const PERSON_SCHEMA = object(_defaultObject)
+  .shape({
+    user: string().transform(value => (value === null ? "" : value)),
+  })
+  .from("userCode", "user", false)
+
+const PERSON_FORM_SCHEMA = object(_defaultObject)
+  .shape({
+    userCode: string().transform(value => (value === null ? "" : value)),
+  })
+  .from("user", "userCode", false)
+
+const toPerson = async personForm => {
+  const isPersonForm = await PERSON_FORM_SCHEMA.isValid(personForm)
+  if (isPersonForm) return PERSON_SCHEMA.cast(personForm)
+  return null
+}
+
+const toPersonForm = async person => {
+  const isPersonForm = await PERSON_FORM_SCHEMA.isValid(person)
+  const isPerson = await PERSON_SCHEMA.isValid(person)
+  if (isPerson) return PERSON_FORM_SCHEMA.cast(person)
+  if (isPersonForm) return person
+  return null
+}
+
+export {PERSON_SCHEMA, PERSON_FORM_SCHEMA, toPerson, toPersonForm}
