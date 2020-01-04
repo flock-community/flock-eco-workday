1c193651dd0eb1df0ea4e51205aa7ff2df779474
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2020-01-04 23:11:07 +0100

    create RandomNumber generator utils function
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

---
+++ src/test/kotlin/utils/RandomNumber.kt
@@ -0,0 +1,10 @@
+package community.flock.eco.workday.utils
+
+/**
+ * @param start <code>Int</code> range start. default 1
+ * @param end <code>Int</code> range end. default 10
+ * @return random generated number from range
+ */
+fun randomNumber(start: Int = 1, end: Int = 10): Int {
+    return (start..end).shuffled().first()
+}
