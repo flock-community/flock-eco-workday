c298a711b30fad6f76ab404c935fc31a36d9a0e3
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2020-01-05 13:10:13 +0100

    remove styles, makeStyles, useStyles
    + remove unused imports
    + styles are part of the item and not of the list, thus not required
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/main/react/features/holiday/HolidayList.js
+++ src/main/react/features/holiday/HolidayList.js
@@ -1,27 +1,10 @@
 import React, {useEffect, useState} from "react"
 import PropTypes from "prop-types"
-import {Card, makeStyles, Typography} from "@material-ui/core"
-import CardContent from "@material-ui/core/CardContent"
 import Grid from "@material-ui/core/Grid"
 import HolidayClient from "../../clients/HolidayClient"
-import {isDefined} from "../../utils/validation"
 import {HolidayListItem} from "./HolidayListItem"
 
-const useStyles = makeStyles(theme => ({
-  content: {
-    position: "relative",
-    cursor: "pointer",
-  },
-  status: {
-    position: "absolute",
-    top: theme.spacing(2),
-    right: theme.spacing(2),
-  },
-}))
-
 export function HolidayList(props) {
-  const classes = useStyles()
-
   const {personCode, refresh, onSelectItem} = props
   const [list, setList] = useState([])
 
