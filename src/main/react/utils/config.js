0a1f890d2fc585fb31fa182283821d91aed44b7e
Reinier Boon <reinier@reinierboon.com>
2020-05-01 12:03:25 +0200

    chore: removed config file since we are only setting theme. which is already done in Application.js

--- src/main/react/utils/config.js
+++
@@ -1,8 +0,0 @@
-import {MuiThemeProvider} from "@material-ui/core/styles"
-import lightTheme from "../theme/theme-light"
-
-export const config = function() {
-  MuiThemeProvider.defaultProps = {
-    theme: lightTheme,
-  }
-}
