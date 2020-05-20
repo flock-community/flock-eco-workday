4b12b0ba656211844c48e28b7064f2e99a22d313
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2020-05-20 07:32:00 +0200

    fix: update status workday

--- src/main/react/features/workday/WorkDayFeature.js
+++ src/main/react/features/workday/WorkDayFeature.js
@@ -61,6 +61,7 @@ export function WorkDayFeature() {
       to: it.to.format("YYYY-MM-DD"),
       status,
       assignmentCode: it.assignment.code,
+      days: it.days.length > 0 ? it.days : null,
     })
       .then(() => setRefresh(!refresh))
       .catch(err => console.log(err))
