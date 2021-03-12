70559c557a7726f74cb67f771d2422a8e1bcd58d
Willem Veelenturf <willem.veelenturf@gmail.com>
2021-03-12 13:09:30 +0100

    Format

--- src/main/react/features/todo/TodoService.tsx
+++ src/main/react/features/todo/TodoService.tsx
@@ -1,50 +1,50 @@
-import {WorkDayClient} from "../../clients/WorkDayClient";
-import {SickDayClient} from "../../clients/SickDayClient";
-import {HolidayClient} from "../../clients/HolidayClient";
-import {ExpenseClient} from "../../clients/ExpenseClient";
+import { WorkDayClient } from "../../clients/WorkDayClient";
+import { SickDayClient } from "../../clients/SickDayClient";
+import { HolidayClient } from "../../clients/HolidayClient";
+import { ExpenseClient } from "../../clients/ExpenseClient";
 
 const updateStatusWorkDay = async (id, status) => {
-  const res = await WorkDayClient.get(id)
+  const res = await WorkDayClient.get(id);
   await WorkDayClient.put(id, {
     ...res,
     assignmentCode: res.assignment.code,
     status,
     from: res.from.format("YYYY-MM-DD"),
     to: res.to.format("YYYY-MM-DD"),
-  })
-}
+  });
+};
 
 const updateStatusSickDay = async (id, status) => {
-  const res = await SickDayClient.get(id)
+  const res = await SickDayClient.get(id);
   await SickDayClient.put(id, {
     ...res,
     status,
     from: res.from.format("YYYY-MM-DD"),
     to: res.to.format("YYYY-MM-DD"),
-  })
-}
+  });
+};
 
 const updateStatusHoliDay = async (id, status) => {
-  const res = await HolidayClient.get(id)
+  const res = await HolidayClient.get(id);
   await HolidayClient.put(id, {
     ...res,
     status,
     from: res.from.format("YYYY-MM-DD"),
     to: res.to.format("YYYY-MM-DD"),
-    days: res.type === 'HOLIDAY' ? res.days : undefined
-  })
-}
+    days: res.type === "HOLIDAY" ? res.days : undefined,
+  });
+};
 
 const updateStatusExpense = async (id, status) => {
-  const res = await ExpenseClient.get(id)
+  const res = await ExpenseClient.get(id);
   await ExpenseClient.put(id, res.type, {
     ...res,
     personId: res.person.uuid,
     status,
     from: res.from.format("YYYY-MM-DD"),
     to: res.to.format("YYYY-MM-DD"),
-  })
-}
+  });
+};
 
 export const updateStatus = (status, item) => {
   if (item.type === "WORKDAY") return updateStatusWorkDay(item.id, status);
