fdd147d5146bff53861894d0d3992c02c89c25d6
Willem Veelenturf <willem.veelenturf@gmail.com>
2021-12-07 10:18:13 +0100

    Fix client hour overview

--- src/main/react/graphql/aggregation.ts
+++ src/main/react/graphql/aggregation.ts
@@ -1,11 +1,21 @@
 export interface AggregationClientPersonOverview {
-  clientName: string;
+  client: AggregationClientPersonOverviewClient;
   aggregationPerson: AggregationClientPersonItem[];
   totals: number[];
 }
 
 export interface AggregationClientPersonItem {
-  personName: string;
+  person: AggregationClientPersonOverviewPerson;
   hours: number[];
   total: number;
+}
+
+export interface AggregationClientPersonOverviewClient {
+  id: string;
+  name: string;
+}
+
+export interface AggregationClientPersonOverviewPerson {
+  id: string;
+  name: string;
 }
