288afbf2e461955dab8daab56c8fab4aae9993cc
Piotr Bryczkowski <piotrbryczkowskiv2@gmail.com>
2021-12-15 11:02:27 +0100

    Added test

--- src/test/kotlin/services/AggregationServiceTest.kt
+++ src/test/kotlin/services/AggregationServiceTest.kt
@@ -9,7 +9,6 @@ import community.flock.eco.workday.model.Assignment
 import community.flock.eco.workday.model.ContractType
 import community.flock.eco.workday.model.WorkDay
 import community.flock.eco.workday.utils.DateUtils.countWorkDaysInMonth
-import org.apache.tomcat.jni.Local
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
@@ -361,24 +360,26 @@ class AggregationServiceTest(
     }
 
     @Test
-    fun `test test`() {
+    fun `total per person me overview`() {
         val person1 = createHelper.createPerson("Jesse", "Pinkman")
         val person2 = createHelper.createPerson("Walter", "White")
         val from = LocalDate.of(2021, 12, 1)
         val to = LocalDate.of(2021, 12, 5)
 
-        /**
-         * from: LocalDate,
-        to: LocalDate,
-        hours: Double? = null,
-        days: List<Double>? = null,
-        persons: List<UUID>) = EventForm
-         */
-
-        val event = createHelper.createEvent(from,to, 40.0, listOf(8.0,8.0,8.0,8.0,8.0), listOf(person1.uuid, person2.uuid))
-        val event2 = createHelper.createEvent(from,to, 40.0, listOf(8.0,8.0,8.0,8.0,8.0), listOf(person2.uuid))
-        //val person = createHelper.createSickDay(person1, from, to)
-      /*  val xd = aggregationService.test(person1.uuid, from, to)
-        val xd2 = xd*/
+        createHelper.createContractExternal(person2, from.minusDays(2), to.plusDays(2))
+        createHelper.createContractInternal(person2, from.minusDays(2), to.plusDays(2))
+        val client = createHelper.createClient()
+        val assignment = createHelper.createAssignment(client, person2, from, to)
+        createHelper.createHoliDay(person2, from, to)
+        createHelper.createWorkDay(assignment, from, to, null, null)
+        createHelper.createEvent(from,to, 40.0, listOf(8.0,8.0,8.0,8.0,8.0), listOf(person1.uuid, person2.uuid))
+        createHelper.createEvent(from,to, 40.0, listOf(8.0,8.0,8.0,8.0,8.0), listOf(person2.uuid))
+        createHelper.createSickDay(person1, from, to)
+        val result = aggregationService.totalPerPerson(from, to, person2)
+        assertEquals(2, result.contractTypes.size)
+        assertEquals(BigDecimal("40.0"), result.workDays)
+        assertEquals(BigDecimal("40.0"), result.workDays)
+        assertEquals(80, result.event)
+        assertEquals(BigDecimal("40.0"), result.holiDayUsed)
     }
 }
