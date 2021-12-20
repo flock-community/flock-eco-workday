e4c7a7d9662516bc3c79078f664255bd7d23ad36
Willem Veelenturf <willem.veelenturf@gmail.com>
2021-12-20 21:55:58 +0100

    Fix redirect

--- src/main/react/application/Application.tsx
+++ src/main/react/application/Application.tsx
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { UserFeature } from "@flock-community/flock-eco-feature-user/src/main/react/user/UserFeature";
-import { CircularProgress, Box } from "@material-ui/core";
+import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom";
+import {UserFeature} from "@flock-community/flock-eco-feature-user/src/main/react/user/UserFeature";
+import {Box, CircularProgress} from "@material-ui/core";
 
-import { makeStyles } from "@material-ui/core/styles";
+import {makeStyles} from "@material-ui/core/styles";
 import ThemeProvider from "@material-ui/styles/ThemeProvider";
 import UserAuthorityUtil from "@flock-community/flock-eco-feature-user/src/main/react/user_utils/UserAuthorityUtil";
-import { HolidayFeature } from "../features/holiday/HolidayFeature";
-import { ApplicationLayout } from "./ApplicationLayout";
-import { ApplicationDrawer } from "./ApplicationDrawer";
-import { ApplicationContext } from "./ApplicationContext";
-import { HomeFeature } from "../features/home/HomeFeature";
-import { ClientFeature } from "../features/client/ClientFeature";
-import { AssignmentFeature } from "../features/assignments/AssignmentFeature";
-import { PersonFeature } from "../features/person/PersonFeature";
-import { SickDayFeature } from "../features/sickday/SickDayFeature";
-import { useUserMe } from "../hooks/UserMeHook";
-import { DashboardFeature } from "../features/dashboard/DashboardFeature";
-import { ContractFeature } from "../features/contract/ContractFeature";
-import { WorkDayFeature } from "../features/workday/WorkDayFeature";
-import { MonthFeature } from "../features/month/MonthFeature";
-import { EventFeature } from "../features/event/EventFeature";
-import { EventRatingFeature } from "../features/event_rating/EventRatingFeature";
-import { useLoginStatus } from "../hooks/StatusHook";
-import { getTheme } from "../theme/theme";
-import { ExpenseFeature } from "../features/expense/ExpenseFeature";
-import { ExactonlineFeature } from "../features/exactonline/ExactonlineFeature";
-import { TodoFeature } from "../features/todo/TodoFeature";
-import { useError } from "../hooks/ErrorHook";
-import { ErrorStack } from "../components/error/ErrorBarStack";
+import {HolidayFeature} from "../features/holiday/HolidayFeature";
+import {ApplicationLayout} from "./ApplicationLayout";
+import {ApplicationDrawer} from "./ApplicationDrawer";
+import {ApplicationContext} from "./ApplicationContext";
+import {HomeFeature} from "../features/home/HomeFeature";
+import {ClientFeature} from "../features/client/ClientFeature";
+import {AssignmentFeature} from "../features/assignments/AssignmentFeature";
+import {PersonFeature} from "../features/person/PersonFeature";
+import {SickDayFeature} from "../features/sickday/SickDayFeature";
+import {useUserMe} from "../hooks/UserMeHook";
+import {DashboardFeature} from "../features/dashboard/DashboardFeature";
+import {ContractFeature} from "../features/contract/ContractFeature";
+import {WorkDayFeature} from "../features/workday/WorkDayFeature";
+import {MonthFeature} from "../features/month/MonthFeature";
+import {EventFeature} from "../features/event/EventFeature";
+import {EventRatingFeature} from "../features/event_rating/EventRatingFeature";
+import {useLoginStatus} from "../hooks/StatusHook";
+import {getTheme} from "../theme/theme";
+import {ExpenseFeature} from "../features/expense/ExpenseFeature";
+import {ExactonlineFeature} from "../features/exactonline/ExactonlineFeature";
+import {TodoFeature} from "../features/todo/TodoFeature";
+import {useError} from "../hooks/ErrorHook";
+import {ErrorStack} from "../components/error/ErrorBarStack";
 
 const useStyles = makeStyles(() => ({
   spinner: {
@@ -69,7 +69,7 @@ export const Application = () => {
   if (!status) {
     return (
       <Box className={classes.spinner}>
-        <CircularProgress />
+        <CircularProgress/>
       </Box>
     );
   }
@@ -86,35 +86,37 @@ export const Application = () => {
   return (
     <ThemeProvider theme={theme}>
       <ApplicationContext.Provider
-        value={{ authorities: status.authorities, user }}
+        value={{authorities: status.authorities, user}}
       >
         <Router>
-          <ApplicationDrawer open={openDrawer} onClose={handleDrawerClose} />
-          <ApplicationLayout onDrawer={handleDrawerOpen} />
+          <ApplicationDrawer open={openDrawer} onClose={handleDrawerClose}/>
+          <ApplicationLayout onDrawer={handleDrawerOpen}/>
           <Box m={2}>
-            <Route path="/" exact component={HomeFeature} />
-            <Route path="/dashboard" exact component={DashboardFeature} />
-            <Route path="/month" exact component={MonthFeature} />
-            <Route path="/todo" exact component={TodoFeature} />
-            <Route path="/clients" exact component={ClientFeature} />
-            <Route path="/contracts" exact component={ContractFeature} />
-            <Route path="/assignments" exact component={AssignmentFeature} />
-            <Route path="/workdays" exact component={WorkDayFeature} />
-            <Route path="/holidays" exact component={HolidayFeature} />
-            <Route path="/sickdays" component={SickDayFeature} />
-            <Route path="/expenses" component={ExpenseFeature} />
-            <Route path="/exactonline" component={ExactonlineFeature} />
-            <Route path="/users" exact component={UserFeature} />
-            <Route path="/person" component={PersonFeature} />
-            <Route path="/event" component={EventFeature} />
-            <Route component={HomeFeature}/>
-            <Route
-              path="/event_rating/:eventCode"
-              component={EventRatingFeature}
-            />
+            <Switch>
+              <Route path="/" exact component={HomeFeature}/>
+              <Route path="/dashboard" exact component={DashboardFeature}/>
+              <Route path="/month" exact component={MonthFeature}/>
+              <Route path="/todo" exact component={TodoFeature}/>
+              <Route path="/clients" exact component={ClientFeature}/>
+              <Route path="/contracts" exact component={ContractFeature}/>
+              <Route path="/assignments" exact component={AssignmentFeature}/>
+              <Route path="/workdays" exact component={WorkDayFeature}/>
+              <Route path="/holidays" exact component={HolidayFeature}/>
+              <Route path="/sickdays" component={SickDayFeature}/>
+              <Route path="/expenses" component={ExpenseFeature}/>
+              <Route path="/exactonline" component={ExactonlineFeature}/>
+              <Route path="/users" exact component={UserFeature}/>
+              <Route path="/person" component={PersonFeature}/>
+              <Route path="/event" component={EventFeature}/>
+              <Route
+                path="/event_rating/:eventCode"
+                component={EventRatingFeature}
+              />
+              <Redirect to='/'/>
+            </Switch>
           </Box>
         </Router>
-        <ErrorStack ErrorList={errors} />
+        <ErrorStack ErrorList={errors}/>
       </ApplicationContext.Provider>
     </ThemeProvider>
   );
