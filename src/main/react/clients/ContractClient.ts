1380caad46dfcf29ecd1645fa1a00b3cfd6105bb
Tim Smelik <timsmelik@gmail.com>
2022-04-07 12:21:38 +0200

    Fix ContractClient

--- src/main/react/clients/ContractClient.ts
+++ src/main/react/clients/ContractClient.ts
@@ -3,6 +3,11 @@ import {Person} from "./PersonClient";
 import InternalizingClient from "../utils/InternalizingClient";
 
 const path = "/api/contracts";
+const internalPath = `${path}-internal`;
+const externalPath = `${path}-external`;
+const managementPath = `${path}-management`;
+const servicePath = `${path}-service`;
+
 
 export type Contract = {
   id: number;
@@ -32,20 +37,33 @@ const internalize = (it) => ({
   to: it.to && moment(it.to, "YYYY-MM-DD"),
 });
 
-const internalizingClient = InternalizingClient<ContactRequest, Contract, ContractRaw>(path, internalize)
+const clients = new Map()
+clients.set("general", InternalizingClient<ContactRequest, Contract, ContractRaw>(path, internalize))
+clients.set("INTERNAL", InternalizingClient<ContactRequest, Contract, ContractRaw>(internalPath, internalize))
+clients.set("EXTERNAL", InternalizingClient<ContactRequest, Contract, ContractRaw>(externalPath, internalize))
+clients.set("MANAGEMENT", InternalizingClient<ContactRequest, Contract, ContractRaw>(managementPath, internalize))
+clients.set("SERVICE", InternalizingClient<ContactRequest, Contract, ContractRaw>(servicePath, internalize))
 
-const findAllByPersonId = (personId: string) => internalizingClient
+const findAllByPersonId = (personId: string) => clients.get("general")
   .query({ personId: personId })
 
 const findAllByToBetween = (start: Date, end: Date) => {
   const startString = start.toISOString().substring(0, 10);
   const endString = end.toISOString().substring(0, 10);
 
-  return internalizingClient.query({ start: startString, end: endString })
+  return clients.get("general").query({ start: startString, end: endString })
 }
 
+const put = (id, type, item) =>
+  clients.get(type).put(id, item);
+
+const post = (type, item) =>
+  clients.get(type).post(item);
+
 export const ContractClient = {
-  ...internalizingClient,
+  ...clients.get("general"),
+  post,
+  put,
   findAllByPersonId,
   findAllByToBetween,
 };
