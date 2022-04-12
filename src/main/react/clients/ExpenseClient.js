c9c35b97934fc0829da6d7049e91ab21eca15d31
Tim Smelik <timsmelik@gmail.com>
2022-04-12 17:19:50 +0200

    Fix expense dates

--- src/main/react/clients/ExpenseClient.js
+++ src/main/react/clients/ExpenseClient.js
@@ -1,9 +1,14 @@
-import moment from "moment";
+import moment, { HTML5_FMT } from "moment";
 import InternalizingClient from "../utils/InternalizingClient.ts";
 
 const internalize = (it) => ({
   ...it,
   date: moment(it.date),
+});
+
+const serialize = (it) => ({
+  ...it,
+  date: typeof it.date === "string" ? it.date : it.date.format(HTML5_FMT.DATE),
 });
 
 const path = "/api/expenses";
@@ -21,22 +26,26 @@ const findAllByPersonId = (personId) =>
   });
 
 const post = (type, item) => {
+  const serialized = serialize(item);
+
   if (type.toUpperCase() === "COST") {
-    return costExpenseClient.post(item);
+    return costExpenseClient.post(serialized);
   }
   if (type.toUpperCase() === "TRAVEL") {
-    return travelExpenseClient.post(item);
+    return travelExpenseClient.post(serialized);
   }
 
   throw Error(`Unknown expense type: ${type}`);
 };
 
 const put = (id, type, item) => {
+  const serialized = serialize(item);
+
   if (type.toUpperCase() === "COST") {
-    return costExpenseClient.put(id, item);
+    return costExpenseClient.put(id, serialized);
   }
   if (type.toUpperCase() === "TRAVEL") {
-    return travelExpenseClient.put(id, item);
+    return travelExpenseClient.put(id, serialized);
   }
 
   throw Error(`Unknown expense type: ${type}`);
