8eed6ae4d395f39ecbb6dee3d638f6494a719d55
Chantalle Mouwer <chantalle.mouwer@flock.community>
2022-09-07 11:38:08 +0200

    Use single name imports only

--- src/main/kotlin/controllers/WorkdayController.kt
+++ src/main/kotlin/controllers/WorkdayController.kt
@@ -16,10 +16,18 @@ import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.Authentication
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 import org.springframework.web.server.ResponseStatusException
-import java.util.*
+import java.util.UUID
 
 @RestController
 @RequestMapping("/api/workdays")
