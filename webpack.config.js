10b17acdf9a2c74a50b71544644403a1ec84ed9b
Florian Putker <florian.putker@flock.community>
2023-01-18 11:40:23 +0100

    I have no idea what this does

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const ecoConfig = require("@flock-community/flock-eco-webpack");
+
+const copyWebpackPlugin = new CopyWebpackPlugin({
+  patterns: [
+    { from: "src/main/react/favicon.ico", to: "./" },
+  ],
+});
 
 const config = {
   ...ecoConfig,
@@ -7,11 +14,16 @@ const config = {
     historyApiFallback: true,
     proxy: {
       ...ecoConfig.devServer.proxy,
+      '/favicon.ico': 'http://localhost:8080',
       '/bootstrap': 'http://localhost:8080',
       '/logout': 'http://localhost:8080',
       '/tasks/*': 'http://localhost:8080',
       '/oauth2/*': 'http://localhost:8080',
     },
   },
+  plugins: [
+    ...ecoConfig.plugins,
+    copyWebpackPlugin,
+  ]
 }
 module.exports = config;
