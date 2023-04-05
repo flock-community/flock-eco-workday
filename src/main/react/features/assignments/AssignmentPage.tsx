fb9f8a549f511c49146c8d5988d4913a74652c1a
Tom Westerhuis <tom.westerhuis@flock.community>
2023-04-05 10:15:08 +0200

    Move add and edit logic inside AssignmentFeature component

--- src/main/react/features/assignments/AssignmentPage.tsx
+++ src/main/react/features/assignments/AssignmentPage.tsx
@@ -1,15 +1,11 @@
 import { AssignmentFeature } from "./AssignmentFeature";
 import React from "react";
 import PersonLayout from "../../components/layouts/PersonLayout";
-import UserAuthorityUtil from "@flock-community/flock-eco-feature-user/src/main/react/user_utils/UserAuthorityUtil";
 
 export default function AssignmentPage() {
   return (
     <PersonLayout requireAuthority={"AssignmentAuthority.ADMIN"}>
-      {(person) => <AssignmentFeature person={person}
-                                      disableEdit={
-                                        !UserAuthorityUtil.hasAuthority("AssignmentAuthority.WRITE")
-                                      }/>}
+      {(person) => <AssignmentFeature person={person} />}
     </PersonLayout>
   );
 }
