353c8a7599f3d268fe2a214ff910577434b4db21
Thomas Machielsen <thomas.machielsen@flock.community>
2023-04-24 14:53:02 +0200

    feat(Tabs): expose the chosen value to the parent component of Tabs

--- src/main/react/components/tabs/Tabs.tsx
+++ src/main/react/components/tabs/Tabs.tsx
@@ -5,18 +5,12 @@ import Grid from "@material-ui/core/Grid";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 
-// types
-import type { TodoItemProps } from "../../features/todo/TodoList";
+// Types
+import { GroupedItemProps } from "../../types";
 
 const TabPanel = ({ children, value, index, ...other }) => {
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
+    <div role="tabpanel" hidden={value !== index} {...other}>
       {value === index && <>{children}</>}
     </div>
   );
@@ -28,13 +22,6 @@ TabPanel.propTypes = {
   value: PropTypes.any.isRequired,
 };
 
-const a11yProps = (index: number) => {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-};
-
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -42,30 +29,32 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-type tabItem = {
-  type: string;
-  items: TodoItemProps[];
-};
-
 type simpleTabsProps = {
-  data: tabItem[];
+  data: GroupedItemProps[];
   renderFunction: Function;
+  exposedValue?: Function;
 };
 
-export const SimpleTabs = ({ data, renderFunction }: simpleTabsProps) => {
+export const SimpleTabs = ({
+  data,
+  renderFunction,
+  exposedValue,
+}: simpleTabsProps) => {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
   const categories = data.map((item) => item.type);
 
   const handleChange = (event: any, newValue: React.SetStateAction<number>) => {
     setValue(newValue);
+
+    exposedValue && exposedValue(newValue);
   };
 
   return (
     <div className={classes.root}>
       <Tabs value={value} onChange={handleChange}>
         {categories.map((category, index) => {
-          return <Tab key={index} label={category} {...a11yProps(index)} />;
+          return <Tab key={index} label={category} />;
         })}
       </Tabs>
       {categories.map((category, index) => {
