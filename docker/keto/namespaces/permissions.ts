ad703ed7a4658963657c5cc9d2d857793dca6654
Julius van Dis <vandis.j@gmail.com>
2023-06-08 20:50:44 +0200

    Add basic Ory setup for local development
    - Kratos for authentication
    - Oathkeeper as identity aware proxy
    - Keto as permission manager
    
    Add the following domains to you /etc/hosts:
    workday.flock.local     127.0.0.1
    accounts.flock.local    127.0.0.1
    
    Navigating to workday.flock.local:8080 requires you to authenticate with Kratos before going into WDA.

---
+++ docker/keto/namespaces/permissions.ts
@@ -0,0 +1,67 @@
+import {Namespace, SubjectSet, Context} from "@ory/keto-namespace-types"
+
+class User implements Namespace {
+
+}
+
+class Flock implements Namespace {
+    related: {
+        organisationalUnits: Flock[]
+        viewers: (User | Flock)[]
+        managers: User[]
+        owners: User[]
+    }
+
+    // if you are a viewer of a Flock, you can view all its organisationalUnits,
+    // if you can view a Person, you can view all owned Workdays of the Person
+    permits = {
+        view: (ctx: Context): boolean =>
+            this.related.viewers.includes(ctx.subject) ||
+            this.related.managers.includes(ctx.subject) ||
+            this.related.organisationalUnits.traverse((f) => f.permits.view(ctx)),
+
+        edit: (ctx: Context): boolean =>
+            this.related.owners.includes(ctx.subject),
+    }
+}
+
+class Person implements Namespace {
+    related: {
+        organisationalUnits: Flock[]
+        owners: User[]
+        managers: User[]
+    }
+
+    // if you can view a Person, you can view all owned Workdays of the Person
+    permits = {
+        view: (ctx: Context): boolean =>
+            this.related.owners.includes(ctx.subject) ||
+            this.related.managers.includes(ctx.subject) ||
+            this.related.organisationalUnits.traverse((f) => f.permits.view(ctx)),
+    }
+}
+
+class Workday implements Namespace {
+    related: {
+        owners: Person[]
+    }
+
+    // if you can view a Person, you can view all owned Workdays of the Person
+    permits = {
+        view: (ctx: Context): boolean =>
+            this.related.owners.traverse((p) => p.permits.view(ctx)),
+    }
+}
+
+
+class Holiday implements Namespace {
+    related: {
+        owners: Person[]
+    }
+
+    // if you can view a Person, you can view all owned Workdays of the Person
+    permits = {
+        view: (ctx: Context): boolean =>
+            this.related.owners.traverse((p) => p.permits.view(ctx)),
+    }
+}
