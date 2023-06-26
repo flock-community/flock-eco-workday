37823631f3d083e2f90cf576d2fecfd51d1a9884
sjorsdev <129753643+sjorsdev@users.noreply.github.com>
2023-06-26 20:46:40 +0200

    fix logout url (#322)
    Co-authored-by: Sjors Engels <sjors.engels@flock.community>

--- src/main/kotlin/config/WebSecurityConfig.kt
+++ src/main/kotlin/config/WebSecurityConfig.kt
@@ -72,6 +72,8 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
                 userSecurityService.googleLogin(http)
                     .and()
                     .defaultSuccessUrl("/", true)
+                    .and()
+                    .logout().logoutSuccessUrl("/")
             "DATABASE" -> userSecurityService.databaseLogin(http).loginPage("/").loginProcessingUrl("/login")
             else -> userSecurityService.testLogin(http).loginPage("/").loginProcessingUrl("/login")
         }
