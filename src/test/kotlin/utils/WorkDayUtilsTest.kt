feed8e2430a0aa97a0078d14fc2bccf2974028e9
Willem Veelenturf <willem.veelenturf@gmail.com>
2023-08-13 10:11:40 +0200

    Bump flock-eco 2.7.11

--- src/test/kotlin/utils/WorkDayUtilsTest.kt
+++ src/test/kotlin/utils/WorkDayUtilsTest.kt
@@ -13,7 +13,7 @@ class WorkDayUtilsTest {
 
     private val startDate = LocalDate.of(2023, 3, 1)
     private val endDate = LocalDate.of(2023, 3, 15)
-    private val days = listOf(8.0,8.0,8.0,0.0,0.0,8.0,8.0,8.0,8.0,8.0,0.0,0.0,8.0,8.0,8.0)
+    private val days = listOf(8.0, 8.0, 8.0, 0.0, 0.0, 8.0, 8.0, 8.0, 8.0, 8.0, 0.0, 0.0, 8.0, 8.0, 8.0)
     private val workday = WorkDay(
         id = 0L,
         hours = 24.0,
@@ -34,7 +34,5 @@ class WorkDayUtilsTest {
         assertEquals("amount of days in 1st week", 5, values[0].size)
         assertEquals("amount of days in 2nd week", 7, values[1].size)
         assertEquals("amount of days in 3th week", 3, values[2].size)
-
-
     }
 }
