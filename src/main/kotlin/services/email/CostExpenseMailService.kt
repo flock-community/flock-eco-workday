c99b2d811b88a73a834a1455927248caff311854
Jos Verhaar <jos.verhaar@flock.community>
2023-08-30 11:15:46 +0200

    WRK-79: Added MailServices for other types

--- src/main/kotlin/services/email/CostExpenseMailService.kt
+++ src/main/kotlin/services/email/CostExpenseMailService.kt
@@ -4,36 +4,38 @@ import community.flock.eco.workday.config.properties.MailjetTemplateProperties
 import community.flock.eco.workday.model.CostExpense
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
 
+@Service
 class CostExpenseMailService(private val emailService: EmailService, private val mailjetTemplateProperties: MailjetTemplateProperties) {
-    private val log: Logger = LoggerFactory.getLogger(MailjetService::class.java);
+    private val log: Logger = LoggerFactory.getLogger(MailjetService::class.java)
 
     fun sendUpdate(old: CostExpense, new: CostExpense) {
-        val recipient = new.person;
-        log.info("Send CostExpense update to ${recipient.email}");
+        val recipient = new.person
+        log.info("Send CostExpense update to ${recipient.email}")
 
-        var subject = "Update in CostExpense!";
-        var emailMessage = "Er is een update in CostExpense.";
+        var subject = "Update in CostExpense!"
+        var emailMessage = "Er is een update in CostExpense."
 
         if (old.status !== new.status) {
-            subject = "Status update in CostExpense!";
+            subject = "Status update in CostExpense!"
             emailMessage = "De status van je CostExpense is veranderd.\n\n" +
                 "vorige status: ${old.status}.\n" +
-                "nieuwe status: ${new.status}.";
+                "nieuwe status: ${new.status}."
         }
 
-        val templateVariables = emailService.createTemplateVariables(recipient.firstname, emailMessage);
-        emailService.sendEmailMessage(recipient.email, subject, templateVariables, mailjetTemplateProperties.updateTemplateId);
+        val templateVariables = emailService.createTemplateVariables(recipient.firstname, emailMessage)
+        emailService.sendEmailMessage(recipient.email, subject, templateVariables, mailjetTemplateProperties.updateTemplateId)
     }
 
     fun sendNotification(expense: CostExpense) {
-        val employee = expense.person;
-        log.info("Send CostExpense notification for ${employee.email}");
+        val employee = expense.person
+        log.info("Send CostExpense notification for ${employee.email}")
 
-        val subject = "Update in CostExpense.";
+        val subject = "Update in CostExpense."
         val emailMessage = "Er is een CostExpense door ${employee.firstname} toegevoegd."
-        val templateVariables = emailService.createTemplateVariables(employee.firstname, emailMessage);
+        val templateVariables = emailService.createTemplateVariables(employee.firstname, emailMessage)
 
-        emailService.sendEmailNotification(subject, templateVariables, mailjetTemplateProperties.notificationTemplateId);
+        emailService.sendEmailNotification(subject, templateVariables, mailjetTemplateProperties.notificationTemplateId)
     }
 }
--- src/main/kotlin/services/email/WorkdayEmailService.kt
+++ src/main/kotlin/services/email/WorkdayEmailService.kt
@@ -7,54 +7,56 @@ import community.flock.eco.workday.model.WorkDay
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.i18n.LocaleContextHolder
+import org.springframework.stereotype.Service
 import java.time.YearMonth
 import java.time.format.TextStyle
 
+@Service
 class WorkdayEmailService(private val emailService: EmailService, private val mailjetTemplateProperties: MailjetTemplateProperties) {
-    private val log: Logger = LoggerFactory.getLogger(MailjetService::class.java);
+    private val log: Logger = LoggerFactory.getLogger(MailjetService::class.java)
 
     fun sendUpdate(old: WorkDay, new: WorkDay) {
-        val recipient = new.assignment.person;
-        log.info("Send workday update to ${recipient.email}");
+        val recipient = new.assignment.person
+        log.info("Send workday update to ${recipient.email}")
 
-        var subject = "Update in Workday.";
-        var emailMessage = "Er is een update in Workday.";
+        var subject = "Update in Workday."
+        var emailMessage = "Er is een update in Workday."
 
         if (old.status !== new.status) {
-            subject = "Status update in Workday!";
+            subject = "Status update in Workday!"
             emailMessage = "De status van je Workday is veranderd.\n\n" +
                 "Vorige status: ${old.status}.\n" +
-                "Nieuwe status: ${new.status}.";
+                "Nieuwe status: ${new.status}."
         }
 
-        val templateVariables = emailService.createTemplateVariables(recipient.firstname, emailMessage);
-        emailService.sendEmailMessage(recipient.email, subject, templateVariables, mailjetTemplateProperties.updateTemplateId);
+        val templateVariables = emailService.createTemplateVariables(recipient.firstname, emailMessage)
+        emailService.sendEmailMessage(recipient.email, subject, templateVariables, mailjetTemplateProperties.updateTemplateId)
     }
 
     fun sendNotification(workDay: WorkDay) {
         if (workDay.status == Status.REQUESTED) {
-            val employee = workDay.assignment.person;
-            val month = YearMonth.from(workDay.from).month.getDisplayName(TextStyle.FULL, LocaleContextHolder.getLocale());
-            log.info("Send workday notification for ${employee.email}");
+            val employee = workDay.assignment.person
+            val month = YearMonth.from(workDay.from).month.getDisplayName(TextStyle.FULL, LocaleContextHolder.getLocale())
+            log.info("Send workday notification for ${employee.email}")
 
             val subject = "Update in Workday."
-            val emailMessage = "Er is een update van ${employee.firstname} in uren van ${month}.";
-            val templateVariables = emailService.createTemplateVariables(employee.firstname, emailMessage);
+            val emailMessage = "Er is een update van ${employee.firstname} in uren van $month."
+            val templateVariables = emailService.createTemplateVariables(employee.firstname, emailMessage)
 
-            emailService.sendEmailNotification(subject, templateVariables, mailjetTemplateProperties.notificationTemplateId);
+            emailService.sendEmailNotification(subject, templateVariables, mailjetTemplateProperties.notificationTemplateId)
         }
     }
 
     fun sendReminder(recipient: Person) {
-        val previousMonth = YearMonth.now().minusMonths(1);
-        val monthString = previousMonth.month.getDisplayName(TextStyle.FULL, LocaleContextHolder.getLocale());
-        log.info("Send workday reminder to ${recipient.email}");
+        val previousMonth = YearMonth.now().minusMonths(1)
+        val monthString = previousMonth.month.getDisplayName(TextStyle.FULL, LocaleContextHolder.getLocale())
+        log.info("Send workday reminder to ${recipient.email}")
 
-        val subject = "Please submit your hours for ${monthString}.";
-        val emailMessage = "We didn't receive your hours for ${monthString}.\n" +
-            "Please submit your hours in the Workday App.";
-        val templateVariables = emailService.createTemplateVariables(recipient.firstname, emailMessage);
+        val subject = "Please submit your hours for $monthString."
+        val emailMessage = "We didn't receive your hours for $monthString.\n" +
+            "Please submit your hours in the Workday App."
+        val templateVariables = emailService.createTemplateVariables(recipient.firstname, emailMessage)
 
-        emailService.sendEmailMessage(recipient.email, subject, templateVariables, mailjetTemplateProperties.reminderTemplateId);
+        emailService.sendEmailMessage(recipient.email, subject, templateVariables, mailjetTemplateProperties.reminderTemplateId)
     }
 }
