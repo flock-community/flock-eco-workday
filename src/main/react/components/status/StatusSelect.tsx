42a9d4bd4c04e83ff233fc374c9c2c9295173663
Jos Verhaar <jos.verhaar@flock.community>
2023-09-25 17:12:06 +0200

    WRK-119: Created a Status select component with extracted logic

---
+++ src/main/react/components/status/StatusSelect.tsx
@@ -0,0 +1,40 @@
+import MenuItem from "@material-ui/core/MenuItem";
+import React from "react";
+import Select from "@material-ui/core/Select";
+import {allStatusTransitions, canChangeStatus, filterTransitionsFromByStatus} from "./StatusMethods";
+
+const statusTransitions = [
+    {from: 'REQUESTED', to: 'REQUESTED'},
+    {from: 'APPROVED', to: 'APPROVED'},
+    {from: 'REJECTED', to: 'REJECTED'},
+    {from: 'DONE', to: 'DONE'},
+    ...allStatusTransitions,
+];
+
+export function StatusSelect({onChange, value}) {
+
+    const currentStateOptions = filterTransitionsFromByStatus(value, statusTransitions);
+
+    const renderMenuItem = (item) => {
+        return (
+            <MenuItem value={item} key={item}>{item}</MenuItem>
+        )
+    }
+
+    const handleOnChange = (event) => {
+        event.stopPropagation();
+        const newValue = event.target.value;
+
+        if (canChangeStatus(value, newValue, statusTransitions)) {
+            onChange(newValue);
+        }
+    }
+
+    return (<>
+        {(currentStateOptions && currentStateOptions.length > 0) && (
+            <Select fullWidth value={value} onChange={handleOnChange}>
+                {(currentStateOptions || []).map(it => renderMenuItem(it))}
+            </Select>
+        )}
+    </>);
+}
