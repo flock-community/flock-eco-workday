2f24bd6d6f16572c0749856068734665140990bf
Jos Verhaar <jos.verhaar@flock.community>
2023-10-13 15:06:55 +0200

    feat(Quick links) Added Quick Links component and implemented WorkDayDialog

---
+++ src/main/react/components/quick-links/QuickLinks.tsx
@@ -0,0 +1,56 @@
+import {Box, Card, CardContent, CardHeader} from "@material-ui/core";
+import React, {useState} from "react";
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import {usePerson} from "../../hooks/PersonHook";
+import {WorkDayDialog} from "../../features/workday/WorkDayDialog";
+import {addError} from "../../hooks/ErrorHook";
+
+export function QuickLinks() {
+    const [person, setPerson] = usePerson();
+    const [value, setValue] = useState<any>()
+    const [workDayOpen, setWorkDayOpen] = useState(false);
+
+    const openAddWorkDay = () => {
+        if (person === null) {
+            addError("No person selected");
+        } else {
+            setValue(undefined);
+            setWorkDayOpen(true);
+        }
+    }
+
+    const handleCLoseWorkdayDialog = () => {
+        setWorkDayOpen(false);
+        setValue(undefined);
+    }
+
+    const openAddLeaveDay = () => {}
+
+    const openAddTravelExpense = () => {}
+
+    const openAddCostExpense = () => {}
+
+    return (
+    <>
+        <Card variant={"outlined"} style={{ borderRadius: 0 }}>
+            <CardHeader title={"Quick links"} />
+            <CardContent>
+                <Box display="flex" justifyContent="space-around">
+                    <Button variant={"contained"} startIcon={<AddIcon/>} onClick={openAddWorkDay} >Workday</Button>
+                    <Button variant={"contained"} startIcon={<AddIcon/>} onClick={openAddLeaveDay} >Leave day</Button>
+                    <Button variant={"contained"} startIcon={<AddIcon/>} onClick={openAddTravelExpense} >Travel expense</Button>
+                    <Button variant={"contained"} startIcon={<AddIcon/>} onClick={openAddCostExpense} >Cost expense</Button>
+                </Box>
+            </CardContent>
+        </Card>
+
+        <WorkDayDialog
+            personFullName={person?.fullName}
+            open={workDayOpen}
+            code={value?.code}
+            onComplete={handleCLoseWorkdayDialog}
+        />
+    </>
+    );
+}
