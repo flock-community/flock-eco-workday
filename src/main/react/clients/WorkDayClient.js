f5c04f55ef4ce82c78c99667cb9841b506c12e86
Willem Veelenturf <willem.veelenturf@ing.com>
2023-10-20 16:35:13 +0200

    rm console.log

--- src/main/react/clients/WorkDayClient.js
+++ src/main/react/clients/WorkDayClient.js
@@ -1,6 +1,5 @@
 import dayjs from "dayjs";
 import InternalizingClient from "../utils/InternalizingClient.ts";
-import { store } from "../hooks/PersonHook";
 
 export const WORK_DAY_PAGE_SIZE = 5;
 
@@ -10,8 +9,6 @@ const internalize = (it) => ({
   to: dayjs(it.to),
   days: it.days && it.days.length === 0 ? null : it.days,
 });
-
-console.log(store);
 
 const path = "/api/workdays";
 const resourceClient = InternalizingClient(path, internalize);
