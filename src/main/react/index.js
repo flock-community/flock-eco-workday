6583c7e26870fd47bdb2bc618fbfad7a1b622efa
Julius van Dis <julius.van.dis@flock.community>
2023-10-26 13:11:00 +0200

    Fix fetch monkey-patch with Context-Person-Id

--- src/main/react/index.js
+++ src/main/react/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Application } from "./application/Application.tsx";
-import {store as personStore }  from "./hooks/PersonHook";
+import { store } from "./hooks/StatusHook";
 
 const { fetch: originalFetch } = window;
 
 window.fetch = async (...args) => {
-  const [resource, config ] = args;
+  const [resource, config] = args;
   const opts = {
     ...config,
-    headers:{
-      "Context-Person-Id": personStore?.id,
-      ...config?.headers
+    headers: {
+      "Context-Person-Id": store && store.personId,
+      ...(config || {}).headers,
     },
-  }
+  };
   const response = await originalFetch(resource, opts);
   return response;
 };
