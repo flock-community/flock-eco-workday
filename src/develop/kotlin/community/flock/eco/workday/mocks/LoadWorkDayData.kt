807f0e0dec25e10c1ebc4833a3645389e053671a
Julius van Dis <julius.van.dis@flock.community>
2023-11-16 10:27:23 +0100

    Sync workdaydata

--- src/develop/kotlin/community/flock/eco/workday/mocks/LoadWorkDayData.kt
+++ src/develop/kotlin/community/flock/eco/workday/mocks/LoadWorkDayData.kt
@@ -1,9 +1,15 @@
 package community.flock.eco.workday.mocks
 
+import community.flock.eco.workday.clients.KetoClientConfiguration
 import community.flock.eco.workday.forms.WorkDayForm
 import community.flock.eco.workday.forms.WorkDaySheetForm
 import community.flock.eco.workday.model.WorkDay
 import community.flock.eco.workday.services.WorkDayService
+import community.flock.wirespec.generated.CreateRelationship
+import community.flock.wirespec.generated.CreateRelationshipBody
+import community.flock.wirespec.generated.SubjectSet
+import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
 import java.time.LocalDate
@@ -12,11 +18,12 @@ import java.util.*
 @Component
 @ConditionalOnProperty(prefix = "flock.eco.workday", name = ["develop"])
 class LoadWorkDayData(
-        private val loadData: LoadData,
-        loadPersonData: LoadPersonData,
-        loadAssignmentData: LoadAssignmentData,
-        private val workDayService: WorkDayService
-) {
+    private val loadData: LoadData,
+    loadPersonData: LoadPersonData,
+    loadAssignmentData: LoadAssignmentData,
+    private val workDayService: WorkDayService,
+    private val ketoClient: KetoClientConfiguration.KetoClient,
+    ) {
     val now = LocalDate.now()
     val data: MutableSet<WorkDay> = mutableSetOf()
 
@@ -24,8 +31,8 @@ class LoadWorkDayData(
      * add a create() function to the WorkDayForm which calls the create function of WorkDayService
      * to create and persist the Workday.
      */
-    private final fun WorkDayForm.create() {
-        loadData.loadWhenEmpty {
+    private final fun WorkDayForm.create(): WorkDay? {
+        return loadData.loadWhenEmpty {
             workDayService.create(this)
         }
     }
@@ -37,8 +44,8 @@ class LoadWorkDayData(
     init {
         val now = LocalDate.now()
         loadAssignmentData.data
-                .map { assignment ->
-                    (1..12)
+            .flatMap { assignment ->
+                    (1..3)
                             .map {
                                 WorkDayForm(
                                         from = now.withMonth(it).withDayOfMonth(1),
@@ -59,7 +66,50 @@ class LoadWorkDayData(
                                 )
                             }
                 }
-                .flatten()
-                .forEach { it.create() }
+            .forEach {
+                it.create()
+                    ?.run {
+                        runBlocking { createRelationBetweenWorkdayAndPerson(this@run) }
+                    }
+            }
+    }
+
+
+//    private fun createKetoRelationForPersonAndUserAccounts(person: Person) {
+//        val kratosUserIds =
+//            (person.user
+//                ?.accounts
+//                ?.filter { ua -> ua is UserAccountOauth && ua.provider == UserAccountOauthProvider.KRATOS } as List<UserAccountOauth>?)
+//                ?.map { it.reference }
+//                ?: emptyList()
+//
+//        kratosUserIds.forEach {
+//            runBlocking { createRelationBetweenPersonAndUser(person, it) }
+//        }
+//    }
+
+    private suspend fun createRelationBetweenWorkdayAndPerson(workday: WorkDay) {
+        val body = CreateRelationshipBody(
+            namespace = "Workday",
+            `object` = workday.code,
+            relation = "owners",
+            subject_set = SubjectSet(
+                namespace = "Person",
+                `object` = workday.assignment.person.uuid.toString(),
+                relation = ""
+            ),
+        )
+
+        val createRelationship = ketoClient.createRelationship(
+            CreateRelationship.RequestApplicationJson(body)
+        )
+        if (createRelationship.status > 299) {
+            error("Some error occurred creating relationship $body. Error: ${createRelationship.content?.body}")
+        }
+        log.info("Created relation between workday {} and person {}", workday.code, workday.assignment.person.uuid)
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(LoadWorkDayData::class.java)
     }
 }
