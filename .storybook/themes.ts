d5fbfceb3231e5160216f0915dd6b123b8311d6d
Jos Verhaar <jos.verhaar@flock.community>
2023-11-23 09:42:09 +0100

    chore(Theming Storybook) Removed the default lightTheme from Storybook

--- .storybook/themes.ts
+++ .storybook/themes.ts
@@ -1,17 +1,6 @@
 import { createTheme } from '@material-ui/core';
 import { blueGrey, cyan, pink } from '@material-ui/core/colors';
 
-export const lightTheme = createTheme({
-  palette: {
-    primary: {
-      main: cyan['A200'],
-    },
-    secondary: {
-      main: pink['A400'],
-    },
-  },
-});
-
 export const darkTheme = createTheme({
   palette: {
     primary: {
