337d211eec1cc3805fd168411a8ce8ef2081835b
Jos Verhaar <jos.verhaar@flock.community>
2023-11-23 09:59:30 +0100

    feat(Holiday Hours Card) WRK-134 Added Storybook stories

--- src/storybook/components/contracts/ContractsEnding.stories.tsx
+++ src/storybook/components/contracts/ContractsEnding.stories.tsx
@@ -1,24 +1,88 @@
-// Button.stories.ts|tsx
-
 import type { Meta, StoryObj } from '@storybook/react';
-
-import { Button } from '@material-ui/core';
 import ContractsEnding from "../../../main/react/components/contracts/ContractsEnding";
+import dayjs from "dayjs";
 
-const meta: Meta<typeof Button> = {
-  component: Button,
+const meta: Meta<typeof ContractsEnding> = {
+  component: ContractsEnding,
+  args: {
+    withinNWeeks: 3,
+    contracts: [
+      {
+        id: 42,
+        code: "a3c4cd4a-6f02-4da5-9ebf-4de4cfef4e55",
+        person: {
+          id: 20,
+          uuid: "96a17fd6-eba7-4e7f-96fc-63d8a2119840",
+          firstname: "Bert",
+          lastname: "Muppets",
+          email: "bert@sesam.straat",
+          position: "",
+          number: "",
+          birthdate: null,
+          joinDate: null,
+          active: true,
+          lastActiveAt: new Date(),
+          reminders: false,
+          shoeSize: "36,5",
+          shirtSize: "M",
+          user: "4dbc2ac4-fc48-4511-a189-bf0d79f7e898",
+          fullName: "Bert Muppets"
+        },
+        from: dayjs("2022-10-16"),
+        to: dayjs("2023-11-24"),
+        hourlyRate: 80.0,
+        hoursPerWeek: 40,
+        billable: true,
+        type: "EXTERNAL"
+      }
+    ]
+  }
 };
 
 export default meta;
-type Story = StoryObj<typeof Button>;
+type Story = StoryObj<typeof ContractsEnding>;
 
-/*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
- * See https://storybook.js.org/docs/react/api/csf
- * to learn how to use render functions.
- */
-export const Primary: Story = {
-  render: () => <>
-    <ContractsEnding withinNWeeks={4}/>
-    </>,
+export const within3weeks: Story = {};
+
+export const within6weeks: Story = {
+  args: {
+    withinNWeeks: 6,
+    contracts: [
+      ...meta.args?.contracts,
+      {
+        id: 42,
+        code: "a3c4cd4a-6f02-4da5-9ebf-4de4cfef4e55",
+        person: {
+          id: 20,
+          uuid: "96a17fd6-eba7-4e7f-96fc-63d8a2119840",
+          firstname: "Bert",
+          lastname: "Muppets",
+          email: "bert@sesam.straat",
+          position: "",
+          number: "",
+          birthdate: null,
+          joinDate: null,
+          active: true,
+          lastActiveAt: new Date(),
+          reminders: false,
+          shoeSize: "36,5",
+          shirtSize: "M",
+          user: "4dbc2ac4-fc48-4511-a189-bf0d79f7e898",
+          fullName: "Bert Muppets"
+        },
+        from: dayjs("2022-10-16"),
+        to: dayjs("2023-11-24"),
+        hourlyRate: 80.0,
+        hoursPerWeek: 40,
+        billable: true,
+        type: "EXTERNAL"
+      }
+    ]
+  }
+};
+
+export const noData: Story = {
+  args: {
+    contracts: []
+  }
 };
---
+++ src/storybook/components/holiday-card/HolidayCard.stories.tsx
@@ -0,0 +1,26 @@
+import {Meta, StoryObj} from "@storybook/react";
+import {HolidayCard} from "../../../main/react/components/holiday-card/HolidayCard";
+
+const meta: Meta<typeof HolidayCard> = {
+  component: HolidayCard,
+  args: {
+    item: {
+      name: 'New Employee',
+      holidayHoursFromContract: 200,
+      plusHours: 0,
+      holidayHoursDone: 24,
+      holidayHoursApproved: 24,
+      holidayHoursRequested: 0,
+      totalHoursAvailable: 208,
+      totalHoursUsed: 48,
+      totalHoursRemaining: 160
+    }
+  }
+}
+
+export default meta;
+type Story = StoryObj<typeof HolidayCard>;
+
+export const withDefaultHours: Story = {
+}
+
