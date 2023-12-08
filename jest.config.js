7fa6003311543255c134722ed102d8f5dbf4ec93
Jos Verhaar <jos.verhaar@flock.community>
2023-12-08 13:38:42 +0100

    feature: Removed Enzyme added @testing-library/jest-dom and @testing-library/user-event | Fixed Jest setup

--- jest.config.js
+++ jest.config.js
@@ -128,15 +128,13 @@ module.exports = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: [
-    "<rootDir>/src/setupTests.js"
-  ],
+  // setupFilesAfterEnv: [],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-jsdom",
+  testEnvironment: "jsdom",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
