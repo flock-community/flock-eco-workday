6afe1a22d64fe08d25896bf59d77bbd093b90ec4
Jos Verhaar <jos.verhaar@flock.community>
2023-12-18 11:16:33 +0100

    styles: Fixed responsiveness of the Quick links component

--- src/main/react/components/quick-links/QuickLinks.tsx
+++ src/main/react/components/quick-links/QuickLinks.tsx
@@ -1,5 +1,5 @@
 import {Box, Card, CardContent, CardHeader, Link} from "@material-ui/core";
-import React, { useState} from "react";
+import React, {useState} from "react";
 import Button from "@material-ui/core/Button";
 import AddIcon from "@material-ui/icons/Add";
 import {usePerson} from "../../hooks/PersonHook";
@@ -9,8 +9,26 @@ import {LeaveDayDialog} from "../../features/holiday/LeaveDayDialog";
 import {ExpenseDialog} from "../../features/expense/ExpenseDialog";
 import {ExpenseType} from "../../features/expense/ExpenseType";
 import {Cloud} from "@material-ui/icons";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  containerWrapper: {
+    containerType: 'inline-size'
+  },
+  buttonWrapper: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    flexWrap: 'wrap',
+    gap: '1rem',
+    '@container (max-width: 500px)': {
+      flexDirection: 'column'
+    }
+  }
+});
 
 export function QuickLinks() {
+  const classes = useStyles();
+
     const [person, setPerson] = usePerson();
     const [value, setValue] = useState<any>()
     const [workDayOpen, setWorkDayOpen] = useState(false);
@@ -64,8 +82,8 @@ export function QuickLinks() {
     <>
         <Card variant={"outlined"} style={{ borderRadius: 0 }}>
             <CardHeader title={"Quick links"} />
-            <CardContent>
-                <Box display="flex" justifyContent="space-around">
+          <CardContent className={classes.containerWrapper}>
+            <Box className={classes.buttonWrapper}>
                     <Button variant={"contained"} startIcon={<AddIcon/>} onClick={openAddWorkDay} >Workday</Button>
                     <Button variant={"contained"} startIcon={<AddIcon/>} onClick={openAddLeaveDay} >Holiday</Button>
                     <Button variant={"contained"} startIcon={<AddIcon/>} onClick={openAddTravelExpense} >Travel expense</Button>
