afce567d18981f75f927349eee9fd927c9c14c4a
Julius van Dis <julius.van.dis@flock.community>
2023-12-27 22:23:13 +0100

    refactor: Improve use of PersoonHook in Application root

--- src/main/react/clients/BootstrapClient.ts
+++ src/main/react/clients/BootstrapClient.ts
@@ -1,17 +1,23 @@
 import {
   checkResponse,
   validateResponse,
-} from "@flock-community/flock-eco-core/src/main/react/clients/utils.ts";
+} from "@flock-community/flock-eco-core/src/main/react/clients/utils";
 
 const path = "/bootstrap";
 
+export type BootstrapData = {
+  authorities: string[];
+  isLoggedIn: boolean;
+  userId?: string;
+  personId?: string;
+};
 const getBootstrap = () => {
   const opts = {
     method: "GET",
   };
 
   return fetch(`${path}`, opts)
-    .then(validateResponse)
+    .then(validateResponse<BootstrapData>)
     .then(checkResponse)
     .then((res) => res.body);
 };
