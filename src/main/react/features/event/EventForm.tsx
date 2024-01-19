3641b4d169a9f718849c91cf07f394c7799b64eb
Jos Verhaar <jos.verhaar@flock.community>
2024-01-19 13:40:46 +0100

    feat: Added billable and EventType to EventForm (WRK-166)

--- src/main/react/features/event/EventForm.tsx
+++ src/main/react/features/event/EventForm.tsx
@@ -4,13 +4,16 @@ import * as Yup from "yup";
 import { Field, Form, Formik } from "formik";
 import Grid from "@material-ui/core/Grid";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
-import { TextField } from "formik-material-ui";
+import {CheckboxWithLabel, TextField} from "formik-material-ui";
 import { DatePickerField } from "../../components/fields/DatePickerField";
 import { PersonSelectorField } from "../../components/fields/PersonSelectorField";
 import { PeriodInputField } from "../../components/fields/PeriodInputField";
 import { mutatePeriod } from "../period/Period";
 import dayjs from "dayjs";
 import DayjsUtils from "@date-io/dayjs";
+import {EventTypeSelect} from "./EventTypeSelect";
+import {FormControl} from "@material-ui/core";
+import {EventTypeMappingToBillable} from "../../utils/mappings";
 
 export const EVENT_FORM_ID = "work-day-form";
 
@@ -23,7 +26,10 @@ const schema = Yup.object().shape({
   days: Yup.array().default([8]).nullable(),
   personIds: Yup.array().default([]),
   costs: Yup.number().required().min(0).default(0),
+  type: Yup.string().required("Field required").default("GENERAL_EVENT"),
+  billable: Yup.boolean().default(false)
 });
+
 
 /**
  * @return {null}
@@ -36,58 +42,80 @@ export function EventForm({ value, onSubmit }) {
       from: data.from,
       to: data.to,
       days: data.days,
-      costs: data.costs
+      costs: data.costs,
+      type: data.type,
+      billable: data.billable,
     });
   };
 
-  const renderForm = ({ values }) => (
-    <Form id={EVENT_FORM_ID}>
-      <MuiPickersUtilsProvider utils={DayjsUtils}>
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            <Field
-              name="description"
-              type="text"
-              label="Description"
-              fullWidth
-              component={TextField}
-            />
+  const renderForm = ({values, setFieldValue}) => {
+    const handleEventTypeChange = (newValue) => {
+      setFieldValue("type", newValue);
+      setFieldValue("billable", EventTypeMappingToBillable[newValue]);
+    };
+
+    return (
+      <Form id={EVENT_FORM_ID}>
+        <MuiPickersUtilsProvider utils={DayjsUtils}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Field
+                name="description"
+                type="text"
+                label="Description"
+                fullWidth
+                component={TextField}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Field
+                name="costs"
+                type="number"
+                label="Costs"
+                fullWidth
+                component={TextField}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <PersonSelectorField name="personIds" multiple fullWidth/>
+            </Grid>
+            <Grid item xs={6} style={{marginTop: '1rem'}}>
+              <EventTypeSelect value={values.type} onChange={handleEventTypeChange}/>
+            </Grid>
+            <Grid item xs={6} style={{marginTop: '1rem'}}>
+              <FormControl fullWidth>
+                <Field
+                  name="billable"
+                  type="checkbox"
+                  Label={{label: "Are these hours billable to client?"}}
+                  component={CheckboxWithLabel}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+              <DatePickerField
+                name="from"
+                label="From"
+                maxDate={values.to}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <DatePickerField
+                name="to"
+                label="To"
+                minDate={values.from}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <PeriodInputField name="days" from={values.from} to={values.to}/>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <Field
-              name="costs"
-              type="number"
-              label="Costs"
-              fullWidth
-              component={TextField}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <PersonSelectorField name="personIds" multiple fullWidth />
-          </Grid>
-          <Grid item xs={6}>
-            <DatePickerField
-              name="from"
-              label="From"
-              maxDate={values.to}
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <DatePickerField
-              name="to"
-              label="To"
-              minDate={values.from}
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <PeriodInputField name="days" from={values.from} to={values.to} />
-          </Grid>
-        </Grid>
-      </MuiPickersUtilsProvider>
-    </Form>
-  );
+        </MuiPickersUtilsProvider>
+      </Form>
+    );
+  };
 
   const init = { ...schema.default(), ...mutatePeriod(value) };
   return (
--- src/main/react/utils/mappings.ts
+++ src/main/react/utils/mappings.ts
@@ -6,3 +6,10 @@ export const EventTypeMapping: Record<EventType, string> = {
   [EventType.FLOCK_COMMUNITY_DAY]: 'Flock. Community Day',
   [EventType.CONFERENCE]: 'Conference',
 };
+
+export const EventTypeMappingToBillable: Record<EventType, boolean> = {
+  [EventType.GENERAL_EVENT]: false,
+  [EventType.FLOCK_HACK_DAY]: false,
+  [EventType.FLOCK_COMMUNITY_DAY]: true,
+  [EventType.CONFERENCE]: false,
+}
