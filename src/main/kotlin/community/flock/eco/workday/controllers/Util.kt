d6155cd0139dda7c0918029ba83f75a061cafe4d
Julius van Dis <julius.van.dis@flock.community>
2025-01-21 19:48:29 +0100

    Only allow owners of an expense or admins to read and modify it

--- src/main/kotlin/community/flock/eco/workday/controllers/Util.kt
+++ src/main/kotlin/community/flock/eco/workday/controllers/Util.kt
@@ -1,7 +1,18 @@
 package community.flock.eco.workday.controllers
 
 import community.flock.eco.workday.api.validate
+import community.flock.eco.workday.model.Person
+import org.springframework.security.core.Authentication
 import java.util.UUID
 import community.flock.eco.workday.api.UUID as UUIDApi
 
 fun UUID.produce(): UUIDApi = UUIDApi(toString()).also(UUIDApi::validate)
+
+/**
+ * Checks if the current user (through the provided Authentication) is associated
+ * with the given person
+ *
+ * @param person The person whose user code is being checked.
+ * @return True if the person's user code matches the authentication name; otherwise, false.
+ */
+fun Authentication.isAssociatedWith(person: Person) = person.user?.code == this.name
