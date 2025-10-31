6a2fce7ae28f2f8b6c25216d40d44e7501812f5b
Julius van Dis <julius.van.dis@flock.community>
2025-10-31 18:08:05 +0100

    Complete phase 6.2 of migration

---
+++ workday-core/src/main/react/clients/PageableClient.ts
@@ -0,0 +1,43 @@
+import {QueryParameters, toQueryString, validateResponse} from './utils'
+
+interface Page<T> {
+  list: T[]
+  count: number
+}
+
+interface Pageable {
+  page: number
+  size: number
+  sort: string
+}
+
+export function PageableClient<T>(path: string) {
+  const queryByPage = (
+    pageable: Pageable,
+    queryParameters: QueryParameters,
+  ): Promise<Page<T>> => {
+    const opts = {
+      method: 'GET',
+    }
+
+    const query = toQueryString({...pageable, ...queryParameters})
+
+    return fetch(`${path}?${query}`, opts)
+      .then((it) => validateResponse<T[]>(it))
+      .then((it) => {
+        if (it) {
+          const total = it?.headers.get('x-total')
+          return {
+            list: it.body,
+            count: total ? parseInt(total, 10) : 0,
+          }
+        } else {
+          throw new Error('List not found')
+        }
+      })
+  }
+
+  const findAllByPage = (pageable: Pageable) => queryByPage(pageable, {})
+
+  return {findAllByPage, queryByPage}
+}
---
+++ workday-core/src/main/react/components/index.ts
@@ -0,0 +1,3 @@
+export {AlignedLoader} from './AlignedLoader'
+export {ConfirmDialog} from './ConfirmDialog'
+export {DialogTitleClosable} from './DialogTitleClosable'
