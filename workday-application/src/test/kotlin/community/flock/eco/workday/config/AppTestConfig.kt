b1bc199f93e90aa5bb2d2dba7a89be749aaa5acf
Julius van Dis <julius.van.dis@flock.community>
2025-11-11 12:34:30 +0100

    Add CleanupDbService to improve test database reset logic for H2 database
    This introduces `CleanupDbService` to handle efficient database cleanup in tests, replacing the prior repository-based deletion logic in `WorkdayIntegrationTest`. Enhances reliability and clarity.

--- workday-application/src/test/kotlin/community/flock/eco/workday/config/AppTestConfig.kt
+++ workday-application/src/test/kotlin/community/flock/eco/workday/config/AppTestConfig.kt
@@ -2,11 +2,13 @@ package community.flock.eco.workday.config
 
 import com.google.api.gax.core.CredentialsProvider
 import com.google.auth.Credentials
+import community.flock.eco.workday.utils.CleanupDbService
 import io.mockk.every
 import io.mockk.mockk
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.test.context.ActiveProfiles
+import javax.sql.DataSource
 
 @TestConfiguration
 @ActiveProfiles("test")
@@ -18,4 +20,7 @@ class AppTestConfig {
         every { provider.credentials }.returns(credentials)
         return provider
     }
+
+    @Bean
+    fun cleanupDbService(dataSource: DataSource) = CleanupDbService(dataSource)
 }
