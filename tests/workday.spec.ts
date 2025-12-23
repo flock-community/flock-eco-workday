d52b27c660dd74e922274b468a263c697d1f1edf
Julius van Dis <julius.van.dis@flock.community>
2025-12-23 14:38:47 +0100

    test: update test case with future date for expense validation

--- tests/workday.spec.ts
+++ tests/workday.spec.ts
@@ -56,10 +56,10 @@ test.describe('Workday scenarios', () => {
 
     await When_I_click_the_button(page, 'Add');
     await Then_I_am_on_the_create_expense_page(page);
-    await When_I_fill_in_the_expense_details(page, '10-06-2025', '16.50', 'Some reason');
+    await When_I_fill_in_the_expense_details(page, '10-06-2028', '16.50', 'Some reason');
     // TODO: File upload causes a call to Google storage API, which is not mocked.
     // await When_I_add_a_file(page, 'receipt.jpg');
     await When_I_click_the_button(page, 'Save');
-    await Then_I_see_the_expense_as(page, 'Requested', 'Some reason', 'Date: 10-06-2025', '€ 16,50');
+    await Then_I_see_the_expense_as(page, 'Requested', 'Some reason', 'Date: 10-06-2028', '€ 16,50');
   });
 });
