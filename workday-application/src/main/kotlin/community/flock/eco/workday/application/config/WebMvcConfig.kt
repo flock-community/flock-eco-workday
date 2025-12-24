ea19df392b2e02d5a1decfe0a3601ecb8baa07e3
Julius van Dis <julius.van.dis@flock.community>
2025-12-24 10:18:06 +0100

    refactor: enhance resource caching, update compression configuration, and improve security handling (#429)
    - Add ETag caching mechanism in `WebMvcConfig` to enforce cache revalidation.
    - Adjust security configuration to include `/site.webmanifest` in permitted requests.
    - Refactor and standardize response compression settings across environments.

--- workday-application/src/main/kotlin/community/flock/eco/workday/application/config/WebMvcConfig.kt
+++ workday-application/src/main/kotlin/community/flock/eco/workday/application/config/WebMvcConfig.kt
@@ -2,15 +2,25 @@ package community.flock.eco.workday.application.config
 
 import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.Resource
+import org.springframework.http.CacheControl
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.resource.PathResourceResolver
 import java.io.IOException
 
 class WebMvcConfig : WebMvcConfigurer {
+    val etagCache: MutableMap<String, String> = mutableMapOf()
+
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler("/**")
             .addResourceLocations("classpath:/static/")
+            .setCacheControl(CacheControl.empty().mustRevalidate())
+            .setEtagGenerator { resource ->
+                etagCache.computeIfAbsent(resource.file.path) {
+                    println("Computing cache for ${resource.file.path}. etag: ${resource.contentAsByteArray.contentHashCode()}")
+                    resource.contentAsByteArray.contentHashCode().toString()
+                }
+            }
             .resourceChain(true)
             .addResolver(
                 object : PathResourceResolver() {
@@ -20,7 +30,7 @@ class WebMvcConfig : WebMvcConfigurer {
                         location: Resource,
                     ): Resource {
                         val requestedResource = location.createRelative(resourcePath)
-                        return if (requestedResource.exists() && requestedResource.isReadable()) {
+                        return if (requestedResource.exists() && requestedResource.isReadable) {
                             requestedResource
                         } else {
                             ClassPathResource("/static/index.html")
